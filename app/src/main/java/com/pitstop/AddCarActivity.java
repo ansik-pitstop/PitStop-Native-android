6f0cdeb9aed744147a9cf6bd7746df1d347b3887
David Liu <info@mrdavidliu.me>
2016-02-21 20:20:25 -0500

    offline adding feature added

--- app/src/main/java/com/pitstop/AddCarActivity.java
+++ app/src/main/java/com/pitstop/AddCarActivity.java
@@ -231,6 +231,11 @@ public class AddCarActivity extends AppCompatActivity implements BluetoothManage
         unbindService(serviceConnection);
     }
 
+    @Override
+    public void finish() {
+        super.finish();
+        VIN="";
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
