7005d52a3654ad39a104dc79e23a52efbc686432
Paul Soladoye <psoladoye@gmail.com>
2016-03-09 13:14:23 -0500

    Service detials ui update

--- app/src/main/java/com/pitstop/SelectDealershipActivity.java
+++ app/src/main/java/com/pitstop/SelectDealershipActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
@@ -50,6 +52,21 @@ public class SelectDealershipActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setup();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.select_dealership_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.refresh) {
+            setup();
+        }
+        return true;
     }
 
     @Override
