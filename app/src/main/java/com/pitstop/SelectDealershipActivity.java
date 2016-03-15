83dcaad126615447bd39237d763182a42e749117
Paul Soladoye <psoladoye@gmail.com>
2016-03-15 10:14:26 -0400

    Add log out to dealership screen

--- app/src/main/java/com/pitstop/SelectDealershipActivity.java
+++ app/src/main/java/com/pitstop/SelectDealershipActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,6 +24,7 @@ import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.pitstop.R;
 import com.pitstop.utils.InternetChecker;
 
@@ -63,8 +65,16 @@ public class SelectDealershipActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.refresh) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.refresh) {
             setup();
+        }
+
+        if(id == R.id.log_out) {
+            ParseUser.logOut();
+            navigateToLogin();
         }
         return true;
     }
@@ -128,6 +138,13 @@ public class SelectDealershipActivity extends AppCompatActivity {
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, SplashScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
     public class DealershipAdapter extends RecyclerView.Adapter<DealershipAdapter.ViewHolder> {
