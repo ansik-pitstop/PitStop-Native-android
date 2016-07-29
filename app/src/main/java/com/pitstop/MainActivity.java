96cdd3a43c98dd2a87e71a24ebaa895a6a6064f6
Ben Wu <bw96412@hotmail.com>
2016-07-29 19:33:53 -0400

    removed tabs for no car

--- app/src/main/java/com/pitstop/MainActivity.java
+++ app/src/main/java/com/pitstop/MainActivity.java
@@ -616,6 +616,7 @@ public class MainActivity extends AppCompatActivity implements ObdManager.IBluet
                                         }
                                         findViewById(R.id.main_view).setVisibility(View.GONE);
                                         findViewById(R.id.no_car_text).setVisibility(View.VISIBLE);
+                                        tabLayout.setVisibility(View.GONE);
                                     } else {
                                         if(mainCarIdCopy != -1) {
                                             for (Car car : carList) {
@@ -630,6 +631,7 @@ public class MainActivity extends AppCompatActivity implements ObdManager.IBluet
                                         }
 
                                         findViewById(R.id.no_car_text).setVisibility(View.GONE);
+                                        tabLayout.setVisibility(View.VISIBLE);
                                         callback.setDashboardCar(carList);
                                         carLocalStore.deleteAllCars();
                                         carLocalStore.storeCars(carList);
