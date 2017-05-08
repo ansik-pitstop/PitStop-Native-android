0415ec8b280e6fbfeaf776d0e7ed75d22c38faaa
Ben Wu <bw96412@hotmail.com>
2017-05-07 22:38:31 -0400

    removed dealertest flavor and dealertest source set (#347)

--- app/src/dealerTest/java/com/pitstop/network/RequestCallback.java
+++
@@ -1,8 +0,0 @@
-package com.pitstop.network;
-
-/**
- * Created by Paul Soladoye on 28/04/2016.
- */
-public interface RequestCallback {
-    void done(String response, RequestError requestError);
-}
