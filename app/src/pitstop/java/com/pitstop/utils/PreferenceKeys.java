ca1f801da81d2e7596fa8667d736e7d0affde467
Ben Wu <bw96412@hotmail.com>
2017-05-10 02:17:52 -0400

    added api endpoint selector in login page, also added retrolambda to project

---
+++ app/src/pitstop/java/com/pitstop/utils/PreferenceKeys.java
@@ -0,0 +1,15 @@
+package com.pitstop.utils;
+
+public class PreferenceKeys {
+
+    // Credentials
+    public final static String NAME_CREDENTIALS = "com.pitstop.credentials";
+    public final static String KEY_USER_ID = "com.pitstop.user_id";
+    public final static String KEY_ACCESS_TOKEN = "com.pitstop.access"; // TODO: move this - preferences aren't secure
+    public final static String KEY_REFRESH_TOKEN = "com.pitstop.refresh";
+    public final static String KEY_LOGGED_IN = "com.pitstop.logged_in";
+
+    // Other
+    public static final String KEY_ENDPOINT = "com.pitstop.endpoint";
+
+}
