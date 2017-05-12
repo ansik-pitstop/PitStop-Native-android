cbbece487dbd4c93bd04397a5d698d3a7bf2d199
Ben Wu <bw96412@hotmail.com>
2017-05-12 12:58:54 -0400

    Debug drawer with random vin, clear storage, and logging framework + added rxjava to project (#358)
    Debug drawer with random vin, clear storage, and logging framework + added rxjava to project

--- app/src/pitstop/java/com/pitstop/utils/DateTimeFormatUtil.java
+++ app/src/pitstop/java/com/pitstop/utils/DateTimeFormatUtil.java
@@ -8,34 +8,36 @@ import java.util.Locale;
 
 public class DateTimeFormatUtil {
 
-    private final static SimpleDateFormat sISO8601DateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US);
+    public final static SimpleDateFormat ISO8601_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US);
+    public final static SimpleDateFormat READABLE_DATE_FORMAT = new SimpleDateFormat("EEE, MMMM dd, yyyy", Locale.US);
+    public final static SimpleDateFormat READABLE_TIME_FORMAT = new SimpleDateFormat("kk:mm aa", Locale.US);
+    public final static SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("HH:mm:ss.SSS", Locale.US);
+    public final static SimpleDateFormat ISSUE_DONE_AT_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+
+    public static String format(Calendar calendar, SimpleDateFormat format){
+        if (calendar == null) return null;
+        return format.format(calendar.getTime());
+    }
 
     public static String formatToISO8601(Calendar calendar){
         if (calendar == null) return null;
-        return sISO8601DateFormat.format(calendar.getTime());
+        return ISO8601_FORMAT.format(calendar.getTime());
     }
-
-    // 2016-08-02T17:48:21.457Z
-    private final static SimpleDateFormat sIssueDoneAtDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
 
     public static Calendar formatISO8601ToCalendar(String dateString) throws ParseException{
         if (dateString == null) return null;
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(sIssueDoneAtDateFormat.parse(dateString));
+        calendar.setTime(ISSUE_DONE_AT_DATE_FORMAT.parse(dateString));
         return calendar;
     }
 
-    private final static SimpleDateFormat sReadableDateFormat = new SimpleDateFormat("EEE, MMMM dd, yyyy", Locale.US);
-
     public static String formatToReadableDate(Calendar calendar){
         if (calendar == null) return null;
-        return sReadableDateFormat.format(calendar.getTime());
+        return READABLE_DATE_FORMAT.format(calendar.getTime());
     }
-
-    private final static SimpleDateFormat sReadableTimeFormat = new SimpleDateFormat("kk:mm aa", Locale.US);
 
     public static String formatToReadableTime(Calendar calendar){
         if (calendar == null) return null;
-        return sReadableTimeFormat.format(calendar.getTime());
+        return READABLE_TIME_FORMAT.format(calendar.getTime());
     }
 }
