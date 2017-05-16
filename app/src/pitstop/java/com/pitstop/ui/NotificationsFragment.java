12ab2d28d9df183ef5aeb9093a8118bf27a3b591
Karol <karol@pitstopconnect.com>
2017-05-16 12:05:22 -0400

    Reverted notifications to only loading on creating the view, and reloading since they're not updating very frequently anyway

--- app/src/pitstop/java/com/pitstop/ui/NotificationsFragment.java
+++ app/src/pitstop/java/com/pitstop/ui/NotificationsFragment.java
@@ -78,6 +78,7 @@ public class NotificationsFragment extends Fragment {
         mNotificationsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mNetworkHelper = new NetworkHelper(getApplicationContext());
 
+        fetchNotifications();
         return rootview;
     }
 
@@ -166,16 +167,6 @@ public class NotificationsFragment extends Fragment {
         mNoNotificationsContainer.setVisibility(View.GONE);
         mNotificationsRecyclerView.setVisibility(View.VISIBLE);
         fetchNotifications();
-    }
-
-    //Load notifications if view is opened
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-
-        if (isVisibleToUser && getView() != null){
-            fetchNotifications();
-        }
     }
 
     private class NotificationListAdapter extends RecyclerView.Adapter<NotificationListViewHolder>{
