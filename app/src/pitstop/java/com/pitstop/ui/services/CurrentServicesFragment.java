13e9b543bddeeceab4a529c60ab90e76080db543
Karol <karol@pitstopconnect.com>
2017-05-23 17:27:57 -0400

    set up frontend GUI for issues

--- app/src/pitstop/java/com/pitstop/ui/services/CurrentServicesFragment.java
+++ app/src/pitstop/java/com/pitstop/ui/services/CurrentServicesFragment.java
@@ -2,28 +2,39 @@ package com.pitstop.ui.services;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pitstop.R;
-import com.pitstop.adapters.CurrentServicesAdapter;
+import com.pitstop.application.GlobalApplication;
+import com.pitstop.database.LocalCarAdapter;
 import com.pitstop.database.LocalCarIssueAdapter;
-import com.pitstop.database.UserAdapter;
-import com.pitstop.interactors.GetCurrentServicesUseCase;
-import com.pitstop.interactors.GetCurrentServicesUseCaseImpl;
-import com.pitstop.interactors.GetUserCarUseCase;
-import com.pitstop.interactors.GetUserCarUseCaseImpl;
 import com.pitstop.models.Car;
 import com.pitstop.models.CarIssue;
+import com.pitstop.network.RequestCallback;
+import com.pitstop.network.RequestError;
+import com.pitstop.ui.MainActivity;
+import com.pitstop.ui.issue_detail.IssueDetailsActivity;
+import com.pitstop.utils.MixpanelHelper;
 import com.pitstop.utils.NetworkHelper;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,19 +44,20 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CurrentServicesFragment extends Fragment{
+public class CurrentServicesFragment extends SubServiceFragment {
 
     public static final String TAG = CurrentServicesFragment.class.getSimpleName();
 
     @BindView(R.id.car_issues_list)
     protected RecyclerView carIssueListView;
-    private CurrentServicesAdapter carIssuesAdapter;
+    private CustomAdapter carIssuesAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
-    private UserAdapter userAdapter;
     private LocalCarIssueAdapter carIssueLocalStore;
+    private LocalCarAdapter carLocalStore;
+    private LocalCarAdapter localCarStore;
     private NetworkHelper networkHelper;
     private List<CarIssue> carIssueList = new ArrayList<>();
-    private boolean start = true;
 
     public static CurrentServicesFragment newInstance(){
         CurrentServicesFragment fragment = new CurrentServicesFragment();
@@ -59,8 +71,6 @@ public class CurrentServicesFragment extends Fragment{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         networkHelper = new NetworkHelper(getActivity().getApplicationContext());
-        carIssueLocalStore = new LocalCarIssueAdapter(getActivity().getApplicationContext());
-        userAdapter = new UserAdapter(getActivity().getApplicationContext());
     }
 
     @Override
@@ -68,61 +78,210 @@ public class CurrentServicesFragment extends Fragment{
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_new_services, container, false);
         ButterKnife.bind(this, view);
-        initUI();
+
+        //This must be called so that UI elements are set for SubService
+        super.onCreateView(inflater,container,savedInstanceState);
 
         return view;
     }
 
-
-    //Call whenever you want to completely new UI objects
-    private void initUI(){
-        final Activity activity = this.getActivity();
-        GetUserCarUseCase getUserCarUseCase = new GetUserCarUseCaseImpl(userAdapter, networkHelper);
-        getUserCarUseCase.execute(new GetUserCarUseCase.Callback() {
-            @Override
-            public void onCarRetrieved(Car car) {
-                carIssuesAdapter = new CurrentServicesAdapter(car,carIssueList, activity);
-                carIssueListView.setLayoutManager(new LinearLayoutManager(activity.getApplicationContext()));
-                carIssueListView.setAdapter(carIssuesAdapter);
-                updateUI();
-            }
-
-            @Override
-            public void onError() {
-            }
-        });
-
+    @Override
+    public void setUI(){
+        carIssuesAdapter = new CustomAdapter(dashboardCar, carIssueList, this.getActivity());
+        carIssueListView.setLayoutManager(new LinearLayoutManager(getContext()));
+        carIssueListView.setAdapter(carIssuesAdapter);
+        populateCarIssuesAdapter();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private void populateCarIssuesAdapter() {
+        // Try local store
+//        Log.i(TAG, "DashboardCar id: (Try local store) "+ dashboardCar.getId());
+        if(carIssueLocalStore == null) {
+            carIssueLocalStore = new LocalCarIssueAdapter(getActivity());
+        }
+        List<CarIssue> carIssues = carIssueLocalStore.getAllCarIssues(dashboardCar.getId());
+        if (carIssues.isEmpty() && (dashboardCar.getNumberOfServices() > 0
+                || dashboardCar.getNumberOfRecalls() > 0)) {
+            Log.i(TAG, "No car issues in local store");
 
-        //Update UI any time another activity(non-tab) is finished
-        if (start){ start = false; }
-        else{
-            updateUI();
+            networkHelper.getCarsById(dashboardCar.getId(), new RequestCallback() {
+                @Override
+                public void done(String response, RequestError requestError) {
+                    if (requestError == null) {
+                        try {
+                            dashboardCar.setIssues(CarIssue.createCarIssues(
+                                    new JSONObject(response).getJSONArray("issues"), dashboardCar.getId()));
+                            carIssueList.clear();
+                            carIssueList.addAll(dashboardCar.getActiveIssues());
+                            carIssuesAdapter.notifyDataSetChanged();
+                           // setIssuesCount();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            if (getActivity() != null) {
+                                Toast.makeText(getActivity(),
+                                        "Error retrieving car details", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    } else {
+                        Log.e(TAG, "Load issues error: " + requestError.getMessage());
+                        if (getActivity() != null) {
+                            Toast.makeText(getActivity(),
+                                    "Error retrieving car details", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+        } else {
+            Log.i(TAG, "Trying local store for carIssues");
+            Log.i(TAG, "Number of active issues: " + dashboardCar.getActiveIssues().size());
+            dashboardCar.setIssues(carIssues);
+            carIssueList.clear();
+            carIssueList.addAll(dashboardCar.getActiveIssues());
+            carIssuesAdapter.notifyDataSetChanged();
+        }
+        //carIssuesAdapter.updateTutorial();
+    }
+
+    private class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+        private WeakReference<Activity> activityReference;
+
+        private Car dashboardCar;
+        private List<CarIssue> carIssues;
+        static final int VIEW_TYPE_EMPTY = 100;
+        static final int VIEW_TYPE_TENTATIVE = 101;
+
+        public CustomAdapter(Car dashboardCar, List<CarIssue> carIssues, Activity activity) {
+            this.dashboardCar = dashboardCar;
+            this.carIssues = carIssues;
+            Log.d(TAG, "Car issue list size: " + this.carIssues.size());
+            activityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_issue, parent, false);
+
+            //Set listener for closing issue
+            ImageView imageView = (ImageView)v.findViewById(R.id.image_done_issue);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG,"Close issue clicked");
+                }
+            });
+
+            return new ViewHolder(v);
+        }
+
+        public CarIssue getItem(int position) {
+            return carIssues.get(position);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            //Log.i(TAG,"On bind view holder");
+            if (activityReference.get() == null) return;
+            final Activity activity = activityReference.get();
+
+            int viewType = getItemViewType(position);
+
+            holder.date.setVisibility(View.GONE);
+
+            if (viewType == VIEW_TYPE_EMPTY) {
+                holder.description.setMaxLines(2);
+                holder.description.setText("You have no pending Engine Code, Recalls or Services");
+                holder.title.setText("Congrats!");
+                holder.imageView.setImageDrawable(
+                        ContextCompat.getDrawable(activity, R.drawable.ic_check_circle_green_400_36dp));
+            } else if (viewType == VIEW_TYPE_TENTATIVE) {
+                holder.description.setMaxLines(2);
+                holder.description.setText("Tap to start");
+                holder.title.setText("Book your first tentative service");
+                holder.imageView.setImageDrawable(
+                        ContextCompat.getDrawable(activity, R.drawable.ic_announcement_blue_600_24dp));
+                holder.container.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // removeTutorial();
+                        ((MainActivity)activity).prepareAndStartTutorialSequence();
+                    }
+                });
+            } else {
+                final CarIssue carIssue = carIssues.get(position);
+
+                holder.description.setText(carIssue.getDescription());
+                holder.description.setEllipsize(TextUtils.TruncateAt.END);
+                if (carIssue.getIssueType().equals(CarIssue.RECALL)) {
+                    holder.imageView.setImageDrawable(ContextCompat
+                            .getDrawable(activity, R.drawable.ic_error_red_600_24dp));
+
+                } else if (carIssue.getIssueType().equals(CarIssue.DTC)) {
+                    holder.imageView.setImageDrawable(ContextCompat
+                            .getDrawable(activity, R.drawable.car_engine_red));
+
+                } else if (carIssue.getIssueType().equals(CarIssue.PENDING_DTC)) {
+                    holder.imageView.setImageDrawable(ContextCompat
+                            .getDrawable(activity, R.drawable.car_engine_yellow));
+                } else {
+                    holder.description.setText(carIssue.getDescription());
+                    holder.imageView.setImageDrawable(ContextCompat
+                            .getDrawable(activity, R.drawable.ic_warning_amber_300_24dp));
+                }
+
+                holder.title.setText(String.format("%s %s", carIssue.getAction(), carIssue.getItem()));
+
+                holder.container.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new MixpanelHelper((GlobalApplication)activity.getApplicationContext())
+                                .trackButtonTapped(carIssues.get(position).getItem(), MixpanelHelper.DASHBOARD_VIEW);
+
+                        Intent intent = new Intent(activity, IssueDetailsActivity.class);
+                        intent.putExtra(MainActivity.CAR_EXTRA, dashboardCar);
+                        intent.putExtra(MainActivity.CAR_ISSUE_EXTRA, carIssue);
+                        activity.startActivityForResult(intent, MainActivity.RC_DISPLAY_ISSUE);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            if (carIssues.isEmpty()) {
+                return VIEW_TYPE_EMPTY;
+            } else if (carIssues.get(position).getIssueType().equals(CarIssue.TENTATIVE)) {
+                return VIEW_TYPE_TENTATIVE;
+            }
+            return super.getItemViewType(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            if (carIssues.isEmpty()) {
+                return 1;
+            }
+            return carIssues.size();
+        }
+
+        public  class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView title;
+            public TextView description;
+            public ImageView imageView;
+            public View container;
+            public View date; // Not used here so it is set to GONE
+
+            public ViewHolder(View v) {
+                super(v);
+                title = (TextView) v.findViewById(R.id.title);
+                description = (TextView) v.findViewById(R.id.description);
+                imageView = (ImageView) v.findViewById(R.id.image_icon);
+                container = v.findViewById(R.id.list_car_item);
+                date = v.findViewById(R.id.date);
+            }
         }
     }
 
-    //Call whenever you want the most recent data from the backend
-    private void updateUI(){
-        GetCurrentServicesUseCase getCurrentServices
-                = new GetCurrentServicesUseCaseImpl(userAdapter,carIssueLocalStore,networkHelper);
-
-        getCurrentServices.execute(new GetCurrentServicesUseCase.Callback() {
-            @Override
-            public void onGotCurrentServices(List<CarIssue> currentServices) {
-                carIssueList.clear();
-                carIssueList.addAll(currentServices);
-                carIssuesAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onError() {
-                Toast.makeText(getActivity(),
-                        "Error retrieving car details", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
