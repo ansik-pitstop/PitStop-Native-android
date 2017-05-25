c500cf85e693cd0a8002d65d6f2e3b42b52f0c21
Karol <karol@pitstopconnect.com>
2017-05-25 12:50:30 -0400

    Removed further debug drawer functionality for performance reasons

--- app/src/pitstop/java/com/pitstop/ui/DebugDrawerActivity.java
+++ app/src/pitstop/java/com/pitstop/ui/DebugDrawerActivity.java
@@ -54,7 +54,7 @@ public abstract class DebugDrawerActivity extends AppCompatActivity {
 
     @Override
     public void setContentView(View view) {
-        if (false) {
+        if (true) {
             mDrawerLayout.addView(view, 0);
         } else {
             super.setContentView(view);
@@ -63,7 +63,7 @@ public abstract class DebugDrawerActivity extends AppCompatActivity {
 
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
-        if (false) {
+        if (true) {
             DrawerLayout drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_debug_drawer, null);
             drawerLayout.addView(view, 0, params);
             super.setContentView(drawerLayout);
@@ -76,7 +76,7 @@ public abstract class DebugDrawerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (false){
+        if (true){
             return;
         }
 
