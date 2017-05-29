24104dd058a226ff3dd60e3b4de0890329905054
Karol <karol@pitstopconnect.com>
2017-05-29 11:50:40 -0400

    Adding car repository

--- app/src/pitstop/java/com/pitstop/repositories/CarRepository.java
+++ app/src/pitstop/java/com/pitstop/repositories/CarRepository.java
@@ -1,67 +1,26 @@
 package com.pitstop.repositories;
 
-import com.google.gson.JsonIOException;
 import com.pitstop.database.LocalCarAdapter;
 import com.pitstop.models.Car;
 import com.pitstop.network.RequestCallback;
-import com.pitstop.network.RequestError;
 import com.pitstop.utils.NetworkHelper;
 
-import org.json.JSONException;
-
-import java.util.List;
-
 /**
- * Car repository, use this class to modify, retrieve, and delete car data.
- * Updates data both remotely and locally.
- *
  * Created by Karol Zdebel on 5/26/2017.
  */
 
-public class CarRepository {
+public class CarRepository implements Repository<Car> {
 
-    private static CarRepository INSTANCE;
     private LocalCarAdapter localCarAdapter;
     private NetworkHelper networkHelper;
-
-    public interface CarInsertCallback{
-        void onCarAdded();
-        void onError();
-    }
-
-    public interface CarUpdateCallback{
-        void onCarUpdated();
-        void onError();
-    }
-
-    public interface CarGetCallback{
-        void onCarGot(Car car);
-        void onError();
-    }
-    public interface CarsGetCallback{
-        void onCarsGot(List<Car> cars);
-        void onError();
-    }
-
-    public interface CarDeleteCallback{
-        void onCarDeleted();
-        void onError();
-    }
-
-    public static synchronized CarRepository getInstance(LocalCarAdapter localCarAdapter
-            , NetworkHelper networkHelper) {
-        if (INSTANCE == null) {
-            INSTANCE = new CarRepository(localCarAdapter, networkHelper);
-        }
-        return INSTANCE;
-    }
 
     public CarRepository(LocalCarAdapter localCarAdapter, NetworkHelper networkHelper){
         this.localCarAdapter = localCarAdapter;
         this.networkHelper = networkHelper;
     }
 
-    public boolean insert(Car model, CarInsertCallback callback) {
+    @Override
+    public boolean insert(Car model, RequestCallback callback) {
 
         //Insert locally
         if (localCarAdapter.getCar(model.getId()) == null){
@@ -73,35 +32,13 @@ public class CarRepository {
 
         //Insert to backend
         networkHelper.createNewCar(model.getUserId(),(int)model.getTotalMileage()
-            ,model.getVin(),model.getScannerId(),model.getShopId()
-                ,getInsertCarRequestCallback(callback));
+            ,model.getVin(),model.getScannerId(),model.getShopId(),callback);
 
         return true;
     }
 
-    private RequestCallback getInsertCarRequestCallback(CarInsertCallback callback){
-        //Create corresponding request callback
-        RequestCallback requestCallback = new RequestCallback() {
-            @Override
-            public void done(String response, RequestError requestError) {
-                try {
-                    if (requestError == null){
-                        callback.onCarAdded();
-                    }
-                    else{
-                        callback.onError();
-                    }
-                }
-                catch(JsonIOException e){
-
-                }
-            }
-        };
-
-        return requestCallback;
-    }
-
-    public boolean update(Car model, CarUpdateCallback callback) {
+    @Override
+    public boolean update(Car model, RequestCallback callback) {
 
         //No rows updated, therefore updating car that doesnt exist
         if (localCarAdapter.updateCar(model) == 0){
@@ -109,116 +46,28 @@ public class CarRepository {
         }
 
         //Update backend
-        networkHelper.updateCar(model.getId(),model.getTotalMileage()
-                ,model.getShopId(),getUpdateCarRequestCallback(callback));
+        networkHelper.updateCarMileage(model.getId(),model.getTotalMileage(),callback);
+        networkHelper.updateCarShop(model.getId(),model.getShopId(),callback);
 
         return true;
     }
 
-    private RequestCallback getUpdateCarRequestCallback(CarUpdateCallback callback){
-        //Create corresponding request callback
-        RequestCallback requestCallback = new RequestCallback() {
-            @Override
-            public void done(String response, RequestError requestError) {
-                try {
-                    if (requestError == null){
-                        callback.onCarUpdated();
-                    }
-                    else{
-                        callback.onError();
-                    }
-                }
-                catch(JsonIOException e){
+    @Override
+    public Car get(int id, RequestCallback callback) {
 
-                }
-            }
-        };
-
-        return requestCallback;
-    }
-
-    public List<Car> getCarByUserId(int userId, CarsGetCallback callback ){
-        networkHelper.getCarsByUserId(userId,getCarsRequestCallback(callback));
-        return localCarAdapter.getCarsByUserId(userId);
-    }
-    private RequestCallback getCarsRequestCallback(CarsGetCallback callback){
-        RequestCallback requestCallback = new RequestCallback() {
-            @Override
-            public void done(String response, RequestError requestError) {
-                try {
-                    if (requestError == null){
-                        callback.onCarsGot(Car.createCarsList(response));
-                    }
-                    else{
-                        callback.onError();
-                    }
-                }
-                catch(JSONException e){
-                }
-            }
-        };
-
-        return requestCallback;
-    }
-
-    public Car get(int id, CarGetCallback callback) {
-        networkHelper.getCarsById(id,getGetCarRequestCallback(callback));
         return localCarAdapter.getCar(id);
     }
 
-    private RequestCallback getGetCarRequestCallback(CarGetCallback callback){
-        //Create corresponding request callback
-        RequestCallback requestCallback = new RequestCallback() {
-            @Override
-            public void done(String response, RequestError requestError) {
-                try {
-                    if (requestError == null){
-                        callback.onCarGot(Car.createCar(response));
-                    }
-                    else{
-                        callback.onError();
-                    }
-                }
-                catch(JSONException e){
-
-                }
-            }
-        };
-
-        return requestCallback;
-    }
-
-    public boolean delete(Car model, CarDeleteCallback callback) {
+    @Override
+    public boolean delete(Car model, RequestCallback callback) {
 
         //Check if car exists before deleting
         if (localCarAdapter.getCar(model.getId()) == null)
             return false;
 
         localCarAdapter.deleteCar(model);
-        networkHelper.deleteUserCar(model.getId(),getDeleteCarRequestCallback(callback));
+        networkHelper.deleteUserCar(model.getId(),callback);
 
         return true;
-    }
-
-    private RequestCallback getDeleteCarRequestCallback(CarDeleteCallback callback){
-        //Create corresponding request callback
-        RequestCallback requestCallback = new RequestCallback() {
-            @Override
-            public void done(String response, RequestError requestError) {
-                try {
-                    if (requestError == null){
-                        callback.onCarDeleted();
-                    }
-                    else{
-                        callback.onError();
-                    }
-                }
-                catch(JsonIOException e){
-
-                }
-            }
-        };
-
-        return requestCallback;
     }
 }
