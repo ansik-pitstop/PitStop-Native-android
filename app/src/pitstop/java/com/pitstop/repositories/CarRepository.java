ae23cce6d7a8755dc2986f739e0c36f28579c2e5
Karol <karol@pitstopconnect.com>
2017-05-29 11:50:40 -0400

    Adding car repository

---
+++ app/src/pitstop/java/com/pitstop/repositories/CarRepository.java
@@ -0,0 +1,73 @@
+package com.pitstop.repositories;
+
+import com.pitstop.database.LocalCarAdapter;
+import com.pitstop.models.Car;
+import com.pitstop.network.RequestCallback;
+import com.pitstop.utils.NetworkHelper;
+
+/**
+ * Created by Karol Zdebel on 5/26/2017.
+ */
+
+public class CarRepository implements Repository<Car> {
+
+    private LocalCarAdapter localCarAdapter;
+    private NetworkHelper networkHelper;
+
+    public CarRepository(LocalCarAdapter localCarAdapter, NetworkHelper networkHelper){
+        this.localCarAdapter = localCarAdapter;
+        this.networkHelper = networkHelper;
+    }
+
+    @Override
+    public boolean insert(Car model, RequestCallback callback) {
+
+        //Insert locally
+        if (localCarAdapter.getCar(model.getId()) == null){
+            localCarAdapter.storeCarData(model);
+        }
+        else{
+            return false; //Car already inserted
+        }
+
+        //Insert to backend
+        networkHelper.createNewCar(model.getUserId(),(int)model.getTotalMileage()
+            ,model.getVin(),model.getScannerId(),model.getShopId(),callback);
+
+        return true;
+    }
+
+    @Override
+    public boolean update(Car model, RequestCallback callback) {
+
+        //No rows updated, therefore updating car that doesnt exist
+        if (localCarAdapter.updateCar(model) == 0){
+            return false;
+        }
+
+        //Update backend
+        networkHelper.updateCarMileage(model.getId(),model.getTotalMileage(),callback);
+        networkHelper.updateCarShop(model.getId(),model.getShopId(),callback);
+
+        return true;
+    }
+
+    @Override
+    public Car get(int id, RequestCallback callback) {
+
+        return localCarAdapter.getCar(id);
+    }
+
+    @Override
+    public boolean delete(Car model, RequestCallback callback) {
+
+        //Check if car exists before deleting
+        if (localCarAdapter.getCar(model.getId()) == null)
+            return false;
+
+        localCarAdapter.deleteCar(model);
+        networkHelper.deleteUserCar(model.getId(),callback);
+
+        return true;
+    }
+}
