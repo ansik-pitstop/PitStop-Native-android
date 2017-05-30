52e68f514064199ba714dfda039594e59f552f7f
Karol <karol@pitstopconnect.com>
2017-05-30 14:53:13 -0400

    Implemented AddCarUseCase

--- app/src/pitstop/java/com/pitstop/interactors/AddCarUseCaseImpl.java
+++ app/src/pitstop/java/com/pitstop/interactors/AddCarUseCaseImpl.java
@@ -1,17 +1,44 @@
 package com.pitstop.interactors;
 
+import com.pitstop.database.LocalCarAdapter;
+import com.pitstop.models.Car;
+import com.pitstop.repositories.CarRepository;
+import com.pitstop.utils.NetworkHelper;
+
 /**
  * Created by Karol Zdebel on 5/30/2017.
  */
 
 public class AddCarUseCaseImpl implements AddCarUseCase {
-    @Override
-    public void execute(Callback callback) {
 
+    private LocalCarAdapter localCarAdapter;
+    private NetworkHelper networkHelper;
+    private Car car;
+    private Callback callback;
+
+    public AddCarUseCaseImpl(LocalCarAdapter localCarAdapter, NetworkHelper networkHelper){
+        this.localCarAdapter = localCarAdapter;
+        this.networkHelper = networkHelper;
+    }
+
+    @Override
+    public void execute(Car car, Callback callback) {
+        this.car = car;
+        this.callback = callback;
     }
 
     @Override
     public void run() {
+        CarRepository.getInstance(localCarAdapter,networkHelper).insert(car, new CarRepository.CarInsertCallback() {
+            @Override
+            public void onCarAdded() {
+                callback.onCarAdded();
+            }
 
+            @Override
+            public void onError() {
+                callback.onError();
+            }
+        });
     }
 }
