bc171b1bc23c88ba78b36f68c1de088390abb705
Karol <karol@pitstopconnect.com>
2017-05-30 14:28:03 -0400

    Added more specific callbacks to CarRepository, and added updateCar() method to NetworkHelper

--- app/src/pitstop/java/com/pitstop/repositories/CarRepository.java
+++ app/src/pitstop/java/com/pitstop/repositories/CarRepository.java
@@ -1,9 +1,13 @@
 package com.pitstop.repositories;
 
+import com.google.gson.JsonIOException;
 import com.pitstop.database.LocalCarAdapter;
 import com.pitstop.models.Car;
 import com.pitstop.network.RequestCallback;
+import com.pitstop.network.RequestError;
 import com.pitstop.utils.NetworkHelper;
+
+import org.json.JSONException;
 
 /**
  * Car repository, use this class to modify, retrieve, and delete car data.
@@ -18,6 +22,26 @@ public class CarRepository {
     private LocalCarAdapter localCarAdapter;
     private NetworkHelper networkHelper;
 
+    interface CarInsertCallback{
+        void onCarAdded();
+        void onError();
+    }
+
+    interface CarUpdateCallback{
+        void onCarUpdated();
+        void onError();
+    }
+
+    interface CarGetCallback{
+        void onCarGot(Car car);
+        void onError();
+    }
+
+    interface CarDeleteCallback{
+        void onCarDeleted();
+        void onError();
+    }
+
     public static synchronized CarRepository getInstance(LocalCarAdapter localCarAdapter
             , NetworkHelper networkHelper) {
         if (INSTANCE == null) {
@@ -31,7 +55,7 @@ public class CarRepository {
         this.networkHelper = networkHelper;
     }
 
-    public boolean insert(Car model, RequestCallback callback) {
+    public boolean insert(Car model, CarInsertCallback callback) {
 
         //Insert locally
         if (localCarAdapter.getCar(model.getId()) == null){
@@ -43,12 +67,35 @@ public class CarRepository {
 
         //Insert to backend
         networkHelper.createNewCar(model.getUserId(),(int)model.getTotalMileage()
-            ,model.getVin(),model.getScannerId(),model.getShopId(),callback);
+            ,model.getVin(),model.getScannerId(),model.getShopId()
+                ,getInsertCarRequestCallback(callback));
 
         return true;
     }
 
-    public boolean update(Car model, RequestCallback callback) {
+    private RequestCallback getInsertCarRequestCallback(CarInsertCallback callback){
+        //Create corresponding request callback
+        RequestCallback requestCallback = new RequestCallback() {
+            @Override
+            public void done(String response, RequestError requestError) {
+                try {
+                    if (requestError == null){
+                        callback.onCarAdded();
+                    }
+                    else{
+                        callback.onError();
+                    }
+                }
+                catch(JsonIOException e){
+
+                }
+            }
+        };
+
+        return requestCallback;
+    }
+
+    public boolean update(Car model, CarUpdateCallback callback) {
 
         //No rows updated, therefore updating car that doesnt exist
         if (localCarAdapter.updateCar(model) == 0){
@@ -56,10 +103,32 @@ public class CarRepository {
         }
 
         //Update backend
-        networkHelper.updateCarMileage(model.getId(),model.getTotalMileage(),callback);
-        networkHelper.updateCarShop(model.getId(),model.getShopId(),callback);
+        networkHelper.updateCar(model.getId(),model.getTotalMileage()
+                ,model.getShopId(),getUpdateCarRequestCallback(callback));
 
         return true;
+    }
+
+    private RequestCallback getUpdateCarRequestCallback(CarUpdateCallback callback){
+        //Create corresponding request callback
+        RequestCallback requestCallback = new RequestCallback() {
+            @Override
+            public void done(String response, RequestError requestError) {
+                try {
+                    if (requestError == null){
+                        callback.onCarUpdated();
+                    }
+                    else{
+                        callback.onError();
+                    }
+                }
+                catch(JsonIOException e){
+
+                }
+            }
+        };
+
+        return requestCallback;
     }
 
     public Car get(int id, RequestCallback callback) {
@@ -67,15 +136,59 @@ public class CarRepository {
         return localCarAdapter.getCar(id);
     }
 
-    public boolean delete(Car model, RequestCallback callback) {
+    private RequestCallback getGetCarRequestCallback(CarGetCallback callback){
+        //Create corresponding request callback
+        RequestCallback requestCallback = new RequestCallback() {
+            @Override
+            public void done(String response, RequestError requestError) {
+                try {
+                    if (requestError == null){
+                        callback.onCarGot(Car.createCar(response));
+                    }
+                    else{
+                        callback.onError();
+                    }
+                }
+                catch(JSONException e){
+
+                }
+            }
+        };
+
+        return requestCallback;
+    }
+
+    public boolean delete(Car model, CarDeleteCallback callback) {
 
         //Check if car exists before deleting
         if (localCarAdapter.getCar(model.getId()) == null)
             return false;
 
         localCarAdapter.deleteCar(model);
-        networkHelper.deleteUserCar(model.getId(),callback);
+        networkHelper.deleteUserCar(model.getId(),getDeleteCarRequestCallback(callback));
 
         return true;
+    }
+
+    private RequestCallback getDeleteCarRequestCallback(CarDeleteCallback callback){
+        //Create corresponding request callback
+        RequestCallback requestCallback = new RequestCallback() {
+            @Override
+            public void done(String response, RequestError requestError) {
+                try {
+                    if (requestError == null){
+                        callback.onCarDeleted();
+                    }
+                    else{
+                        callback.onError();
+                    }
+                }
+                catch(JsonIOException e){
+
+                }
+            }
+        };
+
+        return requestCallback;
     }
 }
