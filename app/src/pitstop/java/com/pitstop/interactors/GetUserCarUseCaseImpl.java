4c5a3c7d8e3aab351faf4f6de2cc9767e02fc176
Karol <karol@pitstopconnect.com>
2017-05-30 15:04:38 -0400

    Implemented RemoveCaruseCase

---
+++ app/src/pitstop/java/com/pitstop/interactors/GetUserCarUseCaseImpl.java
@@ -0,0 +1,18 @@
+package com.pitstop.interactors;
+
+/**
+ * Created by Karol Zdebel on 5/30/2017.
+ */
+
+public class GetUserCarUseCaseImpl implements GetUserCarUseCase {
+
+    @Override
+    public void execute(Callback callback) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
