7f5ddf448472c2269a91917197ac6117ca773392
Karol <karol@pitstopconnect.com>
2017-06-05 13:29:06 -0400

    Removed everything other than the injection method from UseCaseComponent class

--- app/src/pitstop/java/com/pitstop/dependency/UseCaseComponent.java
+++ app/src/pitstop/java/com/pitstop/dependency/UseCaseComponent.java
@@ -1,13 +1,5 @@
 package com.pitstop.dependency;
 
-import com.pitstop.interactors.AddCarUseCase;
-import com.pitstop.interactors.GetCurrentServicesUseCase;
-import com.pitstop.interactors.GetDoneServicesUseCase;
-import com.pitstop.interactors.GetUserCarUseCase;
-import com.pitstop.interactors.MarkServiceDoneUseCase;
-import com.pitstop.interactors.RemoveCarUseCase;
-import com.pitstop.interactors.RequestServiceUseCase;
-import com.pitstop.interactors.SetUserCarUseCase;
 import com.pitstop.ui.services.CurrentServicesFragment;
 
 import dagger.Component;
@@ -20,24 +12,6 @@ import dagger.Component;
 @Component(modules = UseCaseModule.class)
 public interface UseCaseComponent {
 
-//    //These should be both changed to presenters down the road, once they're implemented
     void injectUseCases(CurrentServicesFragment fragment);
-
-
-    AddCarUseCase addCarUseCase();
-
-    GetCurrentServicesUseCase getCurrentServicesUseCase();
-
-    GetDoneServicesUseCase getDoneServicesUseCase();
-
-    GetUserCarUseCase getUserCarUseCase();
-
-    MarkServiceDoneUseCase markServiceDoneUseCase();
-
-    RemoveCarUseCase removeCarUseCase();
-
-    RequestServiceUseCase requestServiceUseCase();
-
-    SetUserCarUseCase setUseCarUseCase();
 
 }
