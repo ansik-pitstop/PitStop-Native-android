e63ff3ccbe58f2a8aeef76a0813865b3b69e8a69
Karol <karol@pitstopconnect.com>
2017-06-08 17:14:17 -0400

    Created CheckSmoochmessageSentUseCase

---
+++ app/src/pitstop/java/com/pitstop/interactors/CheckSmoochMessageSentUseCase.java
@@ -0,0 +1,15 @@
+package com.pitstop.interactors;
+
+/**
+ * Created by Karol Zdebel on 6/8/2017.
+ */
+
+public interface CheckSmoochMessageSentUseCase extends Runnable{
+
+    interface Callback{
+        void onGotWhetherSmoochSent(boolean sent);
+        void onError();
+    }
+
+    void execute(Callback callback);
+}
---
+++ app/src/pitstop/java/com/pitstop/interactors/CheckSmoochMessageSentUseCaseImpl.java
@@ -0,0 +1,46 @@
+package com.pitstop.interactors;
+
+import android.os.Handler;
+
+import com.pitstop.database.UserAdapter;
+import com.pitstop.repositories.UserRepository;
+import com.pitstop.utils.NetworkHelper;
+
+/**
+ * Created by Karol Zdebel on 6/8/2017.
+ */
+
+public class CheckSmoochMessageSentUseCaseImpl implements CheckSmoochMessageSentUseCase {
+
+    private UserAdapter userAdapter;
+    private NetworkHelper networkHelper;
+    private Callback callback;
+
+
+    public CheckSmoochMessageSentUseCaseImpl(UserAdapter userAdapter, NetworkHelper networkHelper) {
+        this.userAdapter = userAdapter;
+        this.networkHelper = networkHelper;
+    }
+
+    @Override
+    public void execute(Callback callback) {
+        this.callback = callback;
+        new Handler().post(this);
+    }
+
+    @Override
+    public void run() {
+        UserRepository.getInstance(userAdapter,networkHelper)
+                .isSmoochMessageSent(new UserRepository.IsSmoochMessageSentCallback() {
+                    @Override
+                    public void onIsSmoochSentRetrieved(boolean sent) {
+                        callback.onGotWhetherSmoochSent(sent);
+                    }
+
+                    @Override
+                    public void onError() {
+                        callback.onError();
+                    }
+                });
+    }
+}
