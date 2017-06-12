89d206ea3cf05f19c66225ef67124b1028ab069f
karolzdebel <karolmzdebel@gmail.com>
2017-06-12 13:55:25 -0400

    karol/dagger2-rebase2 (#405)
    * Adding car repository
    
    * Removed Repository interface since its implementation cannot be consistent across all Repository classes.
    
    * Added getInstance() methods to repositories
    
    * Implemented RemoveCaruseCase
    
    * Implemented RemoveCaruseCase
    
    * Added RemoveUserCarUseCase, and RequestServiceUseCase
    
    * Set properties use case won't be used for now as it is not a priority in terms of implementation so I removed it
    
    * Adding car repository
    
    * Added comments to headers
    
    * Added extra functionality to car issues, getUpcomingCarIssues() now passes int as parameter for id
    
    * Removed Repository interface since its implementation cannot be consistent across all Repository classes.
    
    * Added car related use case interactors and base interactor
    
    * Added getInstance() methods to repositories
    
    * Added more specific callbacks to CarRepository, and added updateCar() method to NetworkHelper
    
    * Made interfaces public
    
    * Implemented RemoveCaruseCase
    
    * Implemented RemoveCaruseCase
    
    * Added RemoveUserCarUseCase, and RequestServiceUseCase
    
    * Added RemoveUserCarUseCase, and RequestServiceUseCase
    
    * Implemented SetUserCarUseCase
    
    * Set properties use case won't be used for now as it is not a priority in terms of implementation so I removed it
    
    * Adding car repository
    
    * Removed Repository interface since its implementation cannot be consistent across all Repository classes.
    
    * Added getInstance() methods to repositories
    
    * Implemented RemoveCaruseCase
    
    * Added RemoveUserCarUseCase, and RequestServiceUseCase
    
    * Added RemoveUserCarUseCase, and RequestServiceUseCase
    
    * Implemented SetUserCarUseCase
    
    * Set properties use case won't be used for now as it is not a priority in terms of implementation so I removed it
    
    * Commented out onClick functionality since it is
    
    * Temporarily disabled logging through bluetooth drawer
    
    * Removed further debug drawer functionality for performance reasons
    
    * Removed debug drawer functionality
    
    * Added front-end current issue removal to history. History is not yet updating accordingly.
    
    * Moved HistoryIssueGroupAdapter out of HistoryServiceFragment to reduce complexity.
    
    * Dates in history are now re-ordered.
    
    * Returned DebugDrawer and LogUtils back to normal functionality
    
    * Created modules/components for Repositories
    
    * Applied application wide scope to repository modules and every dependency to them.
    
    * Fixed compilation errors for Dagger.
    
    * Created dagger module and component for use cases
    
    * Use case now injected into CurrentServicesFragment
    
    * Removed everything other than the injection method from UseCaseComponent class
    
    * CurrentServicesAdapter is now injected
    
    * History and Current Services now functioning using dagger injections
    
    * History and Current Services now functioning using dagger injections
    
    * Use cases now use one global handler
    
    * ServiceRequestActivity now uses Dagger for inecting use case
    
    * Fixed errors introduced from rebase
    
    * UpcomingServicesFragment now uses the GetCurrentServicesMap to get all the data it needs to populate its view.
    
    * HistoryIssuegroupAdapter uses context instead of activity.
    
    * Changed scoping to @Singleton temporarily(for repositories), while the dependency tree is evolving custom scopes will be introduced per activity, fragment etc.
    
    * Renamed variable and removed other unused variables.
    
    * Removed injection from CurrentServicesAdapter

---
+++ app/src/pitstop/java/com/pitstop/dependency/UseCaseModule.java
@@ -0,0 +1,82 @@
+package com.pitstop.dependency;
+
+import android.os.Handler;
+
+import com.pitstop.interactors.AddCarUseCase;
+import com.pitstop.interactors.AddCarUseCaseImpl;
+import com.pitstop.interactors.GetCurrentServicesUseCase;
+import com.pitstop.interactors.GetCurrentServicesUseCaseImpl;
+import com.pitstop.interactors.GetDoneServicesUseCase;
+import com.pitstop.interactors.GetDoneServicesUseCaseImpl;
+import com.pitstop.interactors.GetUserCarUseCase;
+import com.pitstop.interactors.GetUserCarUseCaseImpl;
+import com.pitstop.interactors.MarkServiceDoneUseCase;
+import com.pitstop.interactors.MarkServiceDoneUseCaseImpl;
+import com.pitstop.interactors.RemoveCarUseCase;
+import com.pitstop.interactors.RemoveCarUseCaseImpl;
+import com.pitstop.interactors.RequestServiceUseCase;
+import com.pitstop.interactors.RequestServiceUseCaseImpl;
+import com.pitstop.interactors.SetUserCarUseCase;
+import com.pitstop.interactors.SetUserCarUseCaseImpl;
+import com.pitstop.repositories.CarIssueRepository;
+import com.pitstop.repositories.CarRepository;
+import com.pitstop.repositories.UserRepository;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Karol Zdebel on 6/5/2017.
+ */
+
+@Module(includes = {RepositoryModule.class, HandlerModule.class} )
+public class UseCaseModule {
+
+    @Provides
+    AddCarUseCase addCarUseCase(CarRepository carRepository, Handler handler){
+        return new AddCarUseCaseImpl(carRepository, handler);
+    }
+
+    @Provides
+    GetCurrentServicesUseCase getCurrentServicesUseCase(UserRepository userRepository
+            , CarIssueRepository carIssueRepository, Handler handler){
+
+        return new GetCurrentServicesUseCaseImpl(userRepository, carIssueRepository, handler);
+    }
+
+    @Provides
+    GetDoneServicesUseCase getDoneServicesUseCase(UserRepository userRepository
+            , CarIssueRepository carIssueRepository, Handler handler){
+
+        return new GetDoneServicesUseCaseImpl(userRepository, carIssueRepository, handler);
+    }
+
+    @Provides
+    GetUserCarUseCase getUserCarUseCase(UserRepository userRepository, Handler handler){
+        return new GetUserCarUseCaseImpl(userRepository, handler);
+    }
+
+    @Provides
+    MarkServiceDoneUseCase markServiceDoneUseCase(CarIssueRepository carIssueRepository
+            , Handler handler){
+
+        return new MarkServiceDoneUseCaseImpl(carIssueRepository, handler);
+    }
+
+    @Provides
+    RemoveCarUseCase removeCarUseCase(CarRepository carRepository, Handler handler){
+        return new RemoveCarUseCaseImpl(carRepository,handler);
+    }
+
+    @Provides
+    RequestServiceUseCase requestServiceUseCase(CarIssueRepository carIssueRepository
+            , Handler handler){
+        return new RequestServiceUseCaseImpl(carIssueRepository, handler);
+    }
+
+    @Provides
+    SetUserCarUseCase setUseCarUseCase(UserRepository userRepository, Handler handler){
+        return new SetUserCarUseCaseImpl(userRepository, handler);
+    }
+
+}
