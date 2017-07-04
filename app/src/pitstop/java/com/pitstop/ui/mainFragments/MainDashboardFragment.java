b11f68466d418f17904e5a2ff92abbdad683f943
karolzdebel <karolmzdebel@gmail.com>
2017-07-04 17:02:38 -0400

    karol/DashboardFixLoading (#506)
    * Changed from INVISIBLE to GONE
    
    * removed progress dialog completely

--- app/src/pitstop/java/com/pitstop/ui/mainFragments/MainDashboardFragment.java
+++ app/src/pitstop/java/com/pitstop/ui/mainFragments/MainDashboardFragment.java
@@ -2,7 +2,6 @@
 package com.pitstop.ui.mainFragments;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -137,7 +136,8 @@ public class MainDashboardFragment extends CarDataFragment implements MainDashbo
     @BindView(R.id.my_trips_icon)
     ImageView mMyTripsIcon;
 
-    ProgressDialog progressDialog;
+    @BindView(R.id.loading)
+    View loading;
 
     // Models
     private Car dashboardCar;
@@ -200,9 +200,6 @@ public class MainDashboardFragment extends CarDataFragment implements MainDashbo
                 .contextModule(new ContextModule(application))
                 .build();
 
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setCanceledOnTouchOutside(false);
-
         setStaticUI();
         updateUI();
 
@@ -218,7 +215,7 @@ public class MainDashboardFragment extends CarDataFragment implements MainDashbo
 
     @Override
     public void updateUI(){
-        showLoading("Loading...");
+        showLoading();
 
         useCaseComponent.getUserCarUseCase().execute(new GetUserCarUseCase.Callback() {
             @Override
@@ -854,7 +851,7 @@ public class MainDashboardFragment extends CarDataFragment implements MainDashbo
                             } else {
                                 d.dismiss();
                                 ((MainActivity)getActivity()).getBluetoothConnectService().manuallyUpdateMileage = true;
-                                showLoading("Updating Mileage...");
+                                showLoading();
 
                                 //Update mileage in the GUI so it doesn't have to be loaded from network
                                 mMileageText.setText(String.format("%.2f km", mileage));
@@ -903,19 +900,14 @@ public class MainDashboardFragment extends CarDataFragment implements MainDashbo
         updateMileageDialog.show();
     }
 
-    private void showLoading(String loadingMessage) {
-        if (progressDialog != null && !progressDialog.isShowing() && getUserVisibleHint()) {
-            if (loadingMessage != null)
-                progressDialog.setMessage(loadingMessage);
-            progressDialog.show();
-        }
+    private void showLoading() {
+        loading.setVisibility(View.VISIBLE);
     }
 
     private void hideLoading(String toastMessage) {
-        if (progressDialog != null) {
-            progressDialog.dismiss();
-            if (toastMessage != null && getUserVisibleHint())
-                Toast.makeText(getActivity(), toastMessage, Toast.LENGTH_SHORT).show();
+        loading.setVisibility(View.GONE);
+        if (getUserVisibleHint() && toastMessage != null){
+            Toast.makeText(getContext(),toastMessage,Toast.LENGTH_LONG).show();
         }
     }
 }
