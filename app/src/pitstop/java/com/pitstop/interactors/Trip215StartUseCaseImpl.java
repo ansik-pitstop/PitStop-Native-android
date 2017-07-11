6cff0d5f3f35ddfc0c17f72c7ba0b74b45bd3444
Karol <karol@pitstopconnect.com>
2017-07-11 12:05:26 -0400

    incorporated cars mileage into storing trip

--- app/src/pitstop/java/com/pitstop/interactors/Trip215StartUseCaseImpl.java
+++ app/src/pitstop/java/com/pitstop/interactors/Trip215StartUseCaseImpl.java
@@ -3,9 +3,11 @@ package com.pitstop.interactors;
 import android.os.Handler;
 
 import com.pitstop.bluetooth.dataPackages.TripInfoPackage;
+import com.pitstop.models.Car;
 import com.pitstop.models.Trip215;
 import com.pitstop.repositories.Device215TripRepository;
 import com.pitstop.repositories.Repository;
+import com.pitstop.repositories.UserRepository;
 
 /**
  * Created by Karol Zdebel on 7/6/2017.
@@ -14,15 +16,18 @@ import com.pitstop.repositories.Repository;
 public class Trip215StartUseCaseImpl implements Trip215StartUseCase {
 
     private Device215TripRepository device215TripRepository;
+    private UserRepository userRepository;
     private Handler handler;
     private TripInfoPackage tripInfoPackage;
     private long terminalRTCTime;
     private Callback callback;
 
 
-    public Trip215StartUseCaseImpl(Device215TripRepository device215TripRepository, Handler handler){
+    public Trip215StartUseCaseImpl(Device215TripRepository device215TripRepository
+            , UserRepository userRepository, Handler handler){
 
         this.device215TripRepository = device215TripRepository;
+        this.userRepository = userRepository;
         this.handler = handler;
     }
 
@@ -36,27 +41,47 @@ public class Trip215StartUseCaseImpl implements Trip215StartUseCase {
 
     @Override
     public void run() {
-        Trip215 tripStart = convertToTrip215(tripInfoPackage);
-        device215TripRepository.storeTripStart(tripStart, new Repository.Callback<Trip215>() {
+
+        userRepository.getUserCar(new UserRepository.UserGetCarCallback() {
             @Override
-            public void onSuccess(Trip215 data) {
-                if (tripStart.getRtcTime() > terminalRTCTime){
-                    callback.onRealTimeTripStartSuccess();
-                }
-                else{
-                    callback.onHistoricalTripStartSuccess();
-                }
+            public void onGotCar(Car car) {
+
+                Trip215 tripStart = convertToTrip215(tripInfoPackage, car.getTotalMileage());
+
+                device215TripRepository.storeTripStart(tripStart, new Repository.Callback<Trip215>() {
+                    @Override
+                    public void onSuccess(Trip215 data) {
+                        if (tripStart.getRtcTime() > terminalRTCTime){
+                            callback.onRealTimeTripStartSuccess();
+                        }
+                        else{
+                            callback.onHistoricalTripStartSuccess();
+                        }
+                    }
+
+                    @Override
+                    public void onError(int error) {
+                        callback.onError();
+                    }
+                });
+
             }
 
             @Override
-            public void onError(int error) {
+            public void onNoCarSet() {
+                callback.onError();
+            }
+
+            @Override
+            public void onError() {
                 callback.onError();
             }
         });
+
     }
 
-    private Trip215 convertToTrip215(TripInfoPackage tripInfoPackage){
-        return new Trip215(tripInfoPackage.tripId,tripInfoPackage.mileage
+    private Trip215 convertToTrip215(TripInfoPackage tripInfoPackage, double totalMileage){
+        return new Trip215(tripInfoPackage.tripId, totalMileage
                 ,tripInfoPackage.rtcTime,tripInfoPackage.deviceId);
     }
 }
