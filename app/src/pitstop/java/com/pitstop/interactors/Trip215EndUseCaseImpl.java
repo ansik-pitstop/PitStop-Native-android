96a52c8a4520028e40fdb5797f4c454e2516b5b5
Karol <karol@pitstopconnect.com>
2017-07-18 13:35:12 -0400

    Addressed case where trip start never gets posted but trip end does

--- app/src/pitstop/java/com/pitstop/interactors/Trip215EndUseCaseImpl.java
+++ app/src/pitstop/java/com/pitstop/interactors/Trip215EndUseCaseImpl.java
@@ -42,7 +42,7 @@ public class Trip215EndUseCaseImpl implements Trip215EndUseCase {
             public void onSuccess(Trip215 data) {
 
                 //Latest trip does not exist, don't store trip end
-                if (data == null){
+                if (data == null || data.getTripId() != tripInfoPackage.tripId){
                     callback.onStartTripNotFound();
                     return;
                 }
