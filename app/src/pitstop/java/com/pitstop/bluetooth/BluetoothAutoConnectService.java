7a48024a01f172f8df7b93313f9e0ea9024d5e5d
Karol <karol@pitstopconnect.com>
2017-07-26 13:24:34 -0400

    If adding car, connect with device.

--- app/src/pitstop/java/com/pitstop/bluetooth/BluetoothAutoConnectService.java
+++ app/src/pitstop/java/com/pitstop/bluetooth/BluetoothAutoConnectService.java
@@ -781,7 +781,7 @@ public class BluetoothAutoConnectService extends Service implements ObdManager.I
         //If adding car connect to first recognized device, or 212 device
         // without checking vin(even when not adding car)
         else if (parameterPackage.paramType == ParameterPackage.ParamType.VIN
-                && AddCarActivity.addingCarWithDevice){
+                && AddCarActivity.addingCar){
             setFixedUpload();
             deviceIsVerified = true;
             verificationInProgress = false;
