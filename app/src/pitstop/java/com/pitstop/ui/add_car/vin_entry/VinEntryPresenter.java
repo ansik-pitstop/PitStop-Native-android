755db3f83854ac812cdbc746d05858a9b6755da0
Karol <karol@pitstopconnect.com>
2017-08-02 14:31:13 -0400

    loading show() hide() methods are called inside DeviceSearchPresenter

--- app/src/pitstop/java/com/pitstop/ui/add_car/vin_entry/VinEntryPresenter.java
+++ app/src/pitstop/java/com/pitstop/ui/add_car/vin_entry/VinEntryPresenter.java
@@ -65,6 +65,7 @@ public class VinEntryPresenter {
         }
 
         //Add vehicle logic below
+        view.showLoading("Saving Car");
         useCaseComponent.addCarUseCase().execute(vin, mileage, scannerId, scannerName
                 , EventSource.SOURCE_ADD_CAR, new AddCarUseCase.Callback() {
             @Override
@@ -72,6 +73,7 @@ public class VinEntryPresenter {
                 if (view == null) return;
 
                 view.onCarAddedWithShop(car);
+                view.hideLoading("Added Car Successfully");
             }
 
             @Override
@@ -79,16 +81,19 @@ public class VinEntryPresenter {
                 if (view == null) return;
 
                 view.onCarAddedWithoutShop(car);
+                view.hideLoading("Added Car Successfully");
             }
 
             @Override
             public void onError(RequestError error) {
                 if (error.getError().equals(RequestError.ERR_OFFLINE)){
                     view.onErrorAddingCar("Please connect to the internet to add your vehicle.");
+                    view.hideLoading(null);
                 }
                 else{
                     view.onErrorAddingCar("Unexpected error occurred adding car" +
                             ", please restart the app and try again.");
+                    view.hideLoading(null);
                 }
             }
         });
