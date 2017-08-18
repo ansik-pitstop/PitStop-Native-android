6d7797fe38c216535325046a1391f05b50fbde9b
Karol <karol@pitstopconnect.com>
2017-08-18 15:33:06 -0400

    changed order of method declarations inside BluetoothAutoConnectService

--- app/src/pitstop/java/com/pitstop/observer/BluetoothConnectionObservable.java
+++ app/src/pitstop/java/com/pitstop/observer/BluetoothConnectionObservable.java
@@ -37,6 +37,12 @@ public interface BluetoothConnectionObservable extends Subject{
     //Invoked when an observer wants to retrieve the device rtc time
     boolean requestDeviceTime();
 
+    //Invoked when an observers wants to turn the historical data OFF/ON
+    void requestHistoricalDataStateChange(boolean on);
+
+    //Invoked when an observer wants to retrieve all the supported pids along with their values
+    void requestAllPid();
+
     //Request scan for device
     void requestDeviceSearch(boolean urgent, boolean ignoreVerification);
 
