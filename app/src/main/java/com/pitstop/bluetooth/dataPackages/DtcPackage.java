32d1876a3e25b500deb6ca164d3faa551d5bdecf
Karol <karol@pitstopconnect.com>
2017-08-23 11:52:49 -0400

    changed how dtcs are passed to observer

--- app/src/main/java/com/pitstop/bluetooth/dataPackages/DtcPackage.java
+++ app/src/main/java/com/pitstop/bluetooth/dataPackages/DtcPackage.java
@@ -1,9 +1,6 @@
 package com.pitstop.bluetooth.dataPackages;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by Ben Wu on 2016-09-02.
@@ -14,16 +11,6 @@ public class DtcPackage {
     public int dtcNumber;
     public String[] dtcs;
     public String rtcTime;
-
-    public Set<String> getDtcAsSet(){
-
-        Set<String> dtcSet = new HashSet<>();
-        if (dtcs != null){
-            Collections.addAll(dtcSet, dtcs);
-        }
-        return dtcSet;
-
-    }
 
     @Override
     public String toString() {
