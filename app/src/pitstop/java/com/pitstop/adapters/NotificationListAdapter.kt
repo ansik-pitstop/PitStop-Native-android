e5cbfefc31fa16d5468870049008dab915675aa5
Ishan Kapoor <ikapoor@uwaterloo.ca>
2017-09-05 14:29:23 -0400

    started notification re-design

--- app/src/pitstop/java/com/pitstop/adapters/NotificationListAdapter.kt
+++ app/src/pitstop/java/com/pitstop/adapters/NotificationListAdapter.kt
@@ -1,49 +1,41 @@
 package com.pitstop.adapters
 
+import android.app.Notification
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.pitstop.R
-import com.pitstop.ui.Notifications.NotificationFragment
 import com.pitstop.ui.Notifications.NotificationViewHolder
+
+import java.util.LinkedList
 
 /**
  * Created by ishan on 2017-09-05.
  */
 
+class NotificationListAdapter : RecyclerView.Adapter<NotificationViewHolder>() {
 
-
-
-class NotificationListAdapter(internal var notificationList: List<com.pitstop.models.Notification>) : RecyclerView.Adapter<NotificationViewHolder>(){
+    internal var notificationList: List<Notification>? = null
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder? {
-        val view :View = LayoutInflater.from(parent.context).inflate(R.layout.list_item_notification, parent, false)
-        Log.d("Notification Adapter", "Adapting")
 
-        val holder = NotificationViewHolder(view);
-        val position: Int = viewType
-        view.setOnClickListener {
-            NotificationFragment.onNotificationClicked(notificationList[position].title)
-        }
-        return holder
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_notification, parent, false)
+
+
+
+        return null
     }
+
     override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
-        holder.bind(notificationList[position])
-        Log.d("Notification Adapter", "binding");
 
     }
+
+
     override fun getItemCount(): Int {
-        return notificationList.size
+        return notificationList!!.size
+
     }
-
-    override fun getItemViewType(position: Int): Int {
-        return position;
-    }
-
-
-
 }
