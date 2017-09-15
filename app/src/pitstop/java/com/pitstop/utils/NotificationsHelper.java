29390e2535da0daa447bf474a8f2e9099cdad267
Karol <karol@pitstopconnect.com>
2017-09-15 12:44:15 -0400

    send notifications on main thread

--- app/src/pitstop/java/com/pitstop/utils/NotificationsHelper.java
+++ app/src/pitstop/java/com/pitstop/utils/NotificationsHelper.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
@@ -20,40 +22,45 @@ import static com.pitstop.bluetooth.BluetoothAutoConnectService.notifID;
 
 public class NotificationsHelper {
 
-    public static void cancelConnectedNotification(Context context){
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancel(notifID);
+    private static Handler mainHandler = new Handler(Looper.getMainLooper());
 
+    public static void cancelConnectedNotification(Context context){
+        mainHandler.post(() -> {
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancel(notifID);
+        });
     }
 
     public static void sendNotification(Context context, String message, String title){
-        Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_push);
+        mainHandler.post(() -> {
+            Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_push);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_directions_car_white_24dp)
-                        .setLargeIcon(icon)
-                        .setColor(context.getResources().getColor(R.color.highlight))
-                        .setContentTitle(title)
-                        .setContentText(message);
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.ic_directions_car_white_24dp)
+                            .setLargeIcon(icon)
+                            .setColor(context.getResources().getColor(R.color.highlight))
+                            .setContentTitle(title)
+                            .setContentText(message);
 
-        Intent resultIntent = new Intent(context, MainActivity.class);
-        resultIntent.putExtra(MainActivity.FROM_NOTIF, true);
+            Intent resultIntent = new Intent(context, MainActivity.class);
+            resultIntent.putExtra(MainActivity.FROM_NOTIF, true);
 
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
 
-        stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addParentStack(MainActivity.class);
 
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(notifID, mBuilder.build());
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(notifID, mBuilder.build());
+        });
     }
 }
