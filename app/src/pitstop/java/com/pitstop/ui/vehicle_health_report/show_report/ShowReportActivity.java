76f96790602cb3398d6c13d57dcdc26ddef83dba
Karol <karol@pitstopconnect.com>
2017-09-29 18:40:16 -0400

    ShowReportActivity

--- app/src/pitstop/java/com/pitstop/ui/vehicle_health_report/show_report/ShowReportActivity.java
+++ app/src/pitstop/java/com/pitstop/ui/vehicle_health_report/show_report/ShowReportActivity.java
@@ -1,7 +1,6 @@
 package com.pitstop.ui.vehicle_health_report.show_report;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -29,9 +28,11 @@ public class ShowReportActivity extends AppCompatActivity implements ReportHolde
     private EmissionsReportFragment emissionsReportFragment;
     private final String TAG = getClass().getSimpleName();
 
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         Log.d(TAG,"onCreate()");
         View rootView = getLayoutInflater().inflate(R.layout.activity_show_report
                 , findViewById(android.R.id.content));
@@ -51,7 +52,6 @@ public class ShowReportActivity extends AppCompatActivity implements ReportHolde
         if (emissionsReport != null){
             displayEmissionsReport();
         }
-
     }
 
     public void displayHealthReport(){
