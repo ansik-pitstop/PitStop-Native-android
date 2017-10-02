95dfdb5002f7fa8dfc7addf59c32e148b0ec0cc4
Ishan Kapoor <ikapoor@uwaterloo.ca>
2017-10-02 16:17:17 -0400

    viewpager assinged in oncreateview istead of onActivityCreated

--- app/src/pitstop/java/com/pitstop/ui/my_garage/MyGarageFragment.java
+++ app/src/pitstop/java/com/pitstop/ui/my_garage/MyGarageFragment.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.pitstop.R;
@@ -64,7 +65,7 @@ public class MyGarageFragment extends Fragment implements MyGarageView {
     View addCar;
 
     @BindView(R.id.progress)
-    View loadingView;
+    RelativeLayout loadingView;
 
     private MyGaragePresenter presenter;
     private AlertDialog dealershipCallDialog;
