b5bfbf099d6afe503144b123adbb6ef1b44e0882
Karol <karol@pitstopconnect.com>
2017-10-12 15:07:19 -0400

    created GetCarsWithDealershipsUseCase

---
+++ app/src/pitstop/java/com/pitstop/interactors/get/GetCarsWithDealershipsUseCase.kt
@@ -0,0 +1,18 @@
+package com.pitstop.interactors.get
+
+import com.pitstop.interactors.Interactor
+import com.pitstop.models.Car
+import com.pitstop.models.Dealership
+import com.pitstop.network.RequestError
+
+/**
+ * Created by Karol Zdebel on 10/12/2017.
+ */
+interface GetCarsWithDealershipsUseCase: Interactor {
+    interface Callback{
+        fun onGotCarsWithDealerships(data: Map<Car,Dealership>)
+        fun onError(error: RequestError)
+    }
+
+    fun execute(callback: Callback)
+}
---
+++ app/src/pitstop/java/com/pitstop/interactors/get/GetCarsWithDealershipsUseCaseImpl.kt
@@ -0,0 +1,66 @@
+package com.pitstop.interactors.get
+
+import android.os.Handler
+import com.pitstop.models.Car
+import com.pitstop.models.Dealership
+import com.pitstop.models.User
+import com.pitstop.network.RequestError
+import com.pitstop.repositories.CarRepository
+import com.pitstop.repositories.Repository
+import com.pitstop.repositories.ShopRepository
+import com.pitstop.repositories.UserRepository
+
+/**
+ * Created by Karol Zdebel on 10/12/2017.
+ */
+class GetCarsWithDealershipsUseCaseImpl(val userRepository: UserRepository
+                                        , val carRepository: CarRepository, val shopRepository: ShopRepository
+                                        , val useCaseHandler: Handler, val mainHandler: Handler)
+    : GetCarsWithDealershipsUseCase {
+
+    var callback: GetCarsWithDealershipsUseCase.Callback? = null
+
+    override fun execute(callback: GetCarsWithDealershipsUseCase.Callback) {
+        this.callback = callback
+        useCaseHandler.post(this)
+    }
+
+    override fun run() {
+        val map = HashMap<Car,Dealership>()
+        userRepository.getCurrentUser(object : Repository.Callback<User> {
+
+            override fun onSuccess(user: User) {
+
+                carRepository.getCarsByUserId(user.id, object : Repository.Callback<List<Car>>{
+
+                    override fun onSuccess(carList: List<Car>) {
+
+                        shopRepository.getShopsByUserId(user.id,object : Repository.Callback<List<Dealership>>{
+
+                            override fun onSuccess(dealershipList: List<Dealership>) {
+                                for (car in carList){
+                                    dealershipList
+                                            .filter { car.shopId == it.id }
+                                            .forEach { map.put(car, it) }
+                                }
+                                mainHandler.post({callback!!.onGotCarsWithDealerships(map)})
+                            }
+
+                            override fun onError(error: RequestError) {
+                                mainHandler.post({callback!!.onError(error)})
+                            }
+                        })
+                    }
+
+                    override fun onError(error: RequestError) {
+                        mainHandler.post({callback!!.onError(error)})
+                    }
+                })
+            }
+
+            override fun onError(error: RequestError) {
+                mainHandler.post({callback!!.onError(error)})
+            }
+        })
+    }
+}
