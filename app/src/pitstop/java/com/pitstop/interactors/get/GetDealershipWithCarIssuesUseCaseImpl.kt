2b4973b095467d16e7ba640a74fda2a48a02902b
Karol <karol@pitstopconnect.com>
2017-10-16 16:42:55 -0400

    created get dealership and car issues use case

--- app/src/pitstop/java/com/pitstop/interactors/get/GetDealershipWithCarIssuesUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/get/GetDealershipWithCarIssuesUseCaseImpl.kt
@@ -1,14 +1,20 @@
 package com.pitstop.interactors.get
 
 import android.os.Handler
-import com.pitstop.repositories.CarRepository
-import com.pitstop.repositories.UserRepository
+import com.pitstop.models.Car
+import com.pitstop.models.Dealership
+import com.pitstop.models.Settings
+import com.pitstop.models.issue.CarIssue
+import com.pitstop.network.RequestError
+import com.pitstop.repositories.*
 
 /**
  * Created by Karol Zdebel on 10/16/2017.
  */
 class GetDealershipWithCarIssuesUseCaseImpl(val userRepository: UserRepository
-        , val carRepository: CarRepository, val useCaseHandler: Handler, val mainHandler: Handler)
+        , val carRepository: CarRepository, val carIssueRepository: CarIssueRepository
+                                            , val shopRepository: ShopRepository
+                                            , val useCaseHandler: Handler, val mainHandler: Handler)
     : GetDealershipWithCarIssuesUseCase {
 
     private var callback: GetDealershipWithCarIssuesUseCase.Callback? = null
@@ -20,5 +26,43 @@ class GetDealershipWithCarIssuesUseCaseImpl(val userRepository: UserRepository
 
     override fun run() {
 
+        userRepository.getCurrentUserSettings(object: Repository.Callback<Settings>{
+
+            override fun onSuccess(settings: Settings) {
+                carRepository.get(settings.carId, settings.userId, object: Repository.Callback<Car>{
+
+                    override fun onSuccess(car: Car) {
+                        carIssueRepository.getCurrentCarIssues(car.id, object: Repository.Callback<List<CarIssue>>{
+
+                            override fun onSuccess(carIssueList: List<CarIssue>) {
+
+                                shopRepository.get(car.shopId, settings.userId, object: Repository.Callback<Dealership>{
+
+                                    override fun onSuccess(dealership: Dealership) {
+                                        mainHandler.post({callback!!.onGotDealershipAndIssues(dealership, carIssueList)})
+                                    }
+
+                                    override fun onError(error: RequestError) {
+                                        mainHandler.post({callback!!.onError(error)})
+                                    }
+                                })
+                            }
+
+                            override fun onError(error: RequestError) {
+                                mainHandler.post({callback!!.onError(error)})
+                            }
+                        })
+                    }
+
+                    override fun onError(error: RequestError) {
+                        mainHandler.post({callback!!.onError(error)})
+                    }
+                })
+            }
+
+            override fun onError(error: RequestError) {
+                mainHandler.post({callback!!.onError(error)})
+            }
+        })
     }
 }
