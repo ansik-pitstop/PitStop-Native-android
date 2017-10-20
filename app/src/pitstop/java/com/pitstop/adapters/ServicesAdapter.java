32e3f1ea3fe3a5f0c20976e6467b58350c2ae6e8
Karol <karol@pitstopconnect.com>
2017-10-20 18:46:13 -0400

    services redesign cont

--- app/src/pitstop/java/com/pitstop/adapters/ServicesAdapter.java
+++ app/src/pitstop/java/com/pitstop/adapters/ServicesAdapter.java
@@ -54,7 +54,7 @@ public class ServicesAdapter extends RecyclerView.Adapter<ServicesAdapter.ViewHo
 
         final CarIssue carIssue = carIssues.get(position);
 
-        if (selectionMap.get(carIssue) != null)
+        if (selectionMap.containsValue(carIssue))
             holder.checkBox.setChecked(selectionMap.get(carIssue));
         holder.description.setText(carIssue.getDescription());
         holder.description.setEllipsize(TextUtils.TruncateAt.END);
