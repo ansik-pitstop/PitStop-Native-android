cb4c4cd4b97047c9ec257f630024fd2848a82b8c
Karol <karol@pitstopconnect.com>
2017-11-02 13:53:05 -0400

    integrating retrofit & rxjava into use cases and repositories

--- app/src/pitstop/java/com/pitstop/interactors/get/GetCarsWithDealershipsUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/get/GetCarsWithDealershipsUseCaseImpl.kt
@@ -7,10 +7,8 @@ import com.pitstop.models.Dealership
 import com.pitstop.models.Settings
 import com.pitstop.models.User
 import com.pitstop.network.RequestError
-import com.pitstop.repositories.CarRepository
-import com.pitstop.repositories.Repository
-import com.pitstop.repositories.ShopRepository
-import com.pitstop.repositories.UserRepository
+import com.pitstop.repositories.*
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by Karol Zdebel on 10/12/2017.
@@ -33,47 +31,50 @@ class GetCarsWithDealershipsUseCaseImpl(val userRepository: UserRepository
         userRepository.getCurrentUser(object : Repository.Callback<User> {
 
             override fun onSuccess(user: User) {
+                carRepository.getCarsByUserId(user.id)
+                        .doOnNext { carListResponse ->
+                            Log.d(tag, "getCarsByUserId() response: " + carListResponse)
+                            val carList = carListResponse.data
+                            if (carList == null) {
+                                mainHandler.post({onError(RequestError.getUnknownError())})
+                                return@doOnNext
+                            }
+                            userRepository.getCurrentUserSettings(object: Repository.Callback<Settings>{
+                                override fun onSuccess(settings: Settings) {
+                                    if (carList.isEmpty()) mainHandler.post({callback!!.onGotCarsWithDealerships(map)})
+                                    for (c in carList){
+                                        c.isCurrentCar = c.id == settings.carId
+                                        shopRepository.getAllShops(object : Repository.Callback<List<Dealership>>{
 
-                carRepository.getCarsByUserId(user.id, object : Repository.Callback<List<Car>>{
-
-                    override fun onSuccess(carList: List<Car>) {
-                        userRepository.getCurrentUserSettings(object: Repository.Callback<Settings>{
-                            override fun onSuccess(settings: Settings) {
-                                if (carList.isEmpty()) mainHandler.post({callback!!.onGotCarsWithDealerships(map)})
-                                for (c in carList){
-                                    c.isCurrentCar = c.id == settings.carId
-                                    shopRepository.getAllShops(object : Repository.Callback<List<Dealership>>{
-
-                                        override fun onSuccess(dealershipList: List<Dealership>) {
-                                            Log.d(tag,"cars for user: "+carList)
-                                            Log.d(tag,"shops for user: "+dealershipList)
-                                            for (car in carList){
-                                                dealershipList
-                                                        .filter { car.shopId == it.id }
-                                                        .forEach { map.put(car, it) }
+                                            override fun onSuccess(dealershipList: List<Dealership>) {
+                                                Log.d(tag,"cars for user: "+carList)
+                                                Log.d(tag,"shops for user: "+dealershipList)
+                                                for (car in carList){
+                                                    dealershipList
+                                                            .filter { car.shopId == it.id }
+                                                            .forEach { map.put(car, it) }
+                                                }
+                                                Log.d(tag,"Resulting map: "+map)
+                                                mainHandler.post({callback!!.onGotCarsWithDealerships(map)})
                                             }
-                                            Log.d(tag,"Resulting map: "+map)
-                                            mainHandler.post({callback!!.onGotCarsWithDealerships(map)})
-                                        }
 
-                                        override fun onError(error: RequestError) {
-                                            mainHandler.post({callback!!.onError(error)})
-                                        }
-                                    })
+                                            override fun onError(error: RequestError) {
+                                                mainHandler.post({callback!!.onError(error)})
+                                            }
+                                        })
+                                    }
                                 }
-                            }
 
-                            override fun onError(error: RequestError) {
-                                mainHandler.post({callback!!.onError(error)})
-                            }
-                        })
+                                override fun onError(error: RequestError) {
+                                    mainHandler.post({callback!!.onError(error)})
+                                }
+                            })
 
-                    }
-
-                    override fun onError(error: RequestError) {
-                        mainHandler.post({callback!!.onError(error)})
-                    }
-                })
+                        }.onErrorReturn { err ->
+                    Log.d(tag, "getCarsByUserId() err: " + err)
+                    Response<List<Car>>(null, false)
+                }.subscribeOn(Schedulers.io())
+                        .subscribe()
             }
 
             override fun onError(error: RequestError) {
