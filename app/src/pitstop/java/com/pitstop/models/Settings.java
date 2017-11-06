184bd4a3a32377abc920e2395271d0933bb3efc2
Karol <karol@pitstopconnect.com>
2017-11-06 18:19:37 -0500

    CarRepository get() fix

--- app/src/pitstop/java/com/pitstop/models/Settings.java
+++ app/src/pitstop/java/com/pitstop/models/Settings.java
@@ -1,9 +1,5 @@
 package com.pitstop.models;
 
-import android.util.Log;
-
-import java.util.concurrent.Semaphore;
-
 /**
  * Not complete, doesn't include everything stored inside settings
  *
@@ -17,35 +13,24 @@ public class Settings {
     private int userId;
     private int carId;  //User settings car id
     private boolean firstCarAdded;  //Whether user ever added a car
-    private Semaphore semaphore = new Semaphore(1);
 
     public Settings(int userId, int carId, boolean firstCarAdded) {
-        Log.d(TAG,"Settings being created, userId:"+userId+", carId: "+carId+", firstCarId: "+firstCarAdded);
         this.userId = userId;
         this.carId = carId;
         this.firstCarAdded = firstCarAdded;
     }
 
     public Settings(int userId, boolean firstCarAdded){
-        Log.d(TAG,"Settings being created, userId:"+userId+", firstCarId: "+firstCarAdded);
         this.userId = userId;
         this.firstCarAdded = firstCarAdded;
         carId = -1;
     }
 
     public void setCarId(int carId) {
-        Log.d(TAG,"setCarId() carId: "+carId);
-        try{
-            semaphore.acquire();
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
         this.carId = carId;
-        semaphore.release();
     }
 
     public void setFirstCarAdded(boolean firstCarAdded) {
-        Log.d(TAG,"setFirstCarAdded() firstCarAdded: "+firstCarAdded);
         this.firstCarAdded = firstCarAdded;
     }
 
@@ -54,13 +39,6 @@ public class Settings {
     }
 
     public int getCarId() {
-        try{
-            semaphore.acquire();
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
-        Log.d(TAG,"getCardId() carId: "+carId);
-        semaphore.release();
         return carId;
     }
 
