1b23743994f4fc66ffd80bb9f5728f25f9b4a163
Karol <karol@pitstopconnect.com>
2017-11-09 15:47:45 -0500

    settings now sets carId to -1 by default meaning "no car present"

--- app/src/pitstop/java/com/pitstop/database/LocalUserStorage.java
+++ app/src/pitstop/java/com/pitstop/database/LocalUserStorage.java
@@ -69,9 +69,7 @@ public class LocalUserStorage {
         int carId = c.getInt(c.getColumnIndex(TABLES.USER.KEY_CAR));
         boolean isFirstCarAdded = c.getInt(c.getColumnIndex(TABLES.USER.KEY_FIRST_CAR_ADDED)) == 1;
         boolean alarmsEnabled = c.getInt(c.getColumnIndex(TABLES.USER.KEY_ALARMS_ENABLED)) == 1;
-        if (carId != -1){
-            user.setSettings(new Settings(user.getId(),carId,isFirstCarAdded,alarmsEnabled));
-        }
+        user.setSettings(new Settings(user.getId(),carId,isFirstCarAdded,alarmsEnabled));
         return user;
     }
 
