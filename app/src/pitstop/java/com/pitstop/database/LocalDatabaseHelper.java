0cd2fc5e04fc205c024c0676c623e9edff5328d2
Karol <karol@pitstopconnect.com>
2017-11-09 13:57:14 -0500

    local database version upgrade

--- app/src/pitstop/java/com/pitstop/database/LocalDatabaseHelper.java
+++ app/src/pitstop/java/com/pitstop/database/LocalDatabaseHelper.java
@@ -20,7 +20,7 @@ public class LocalDatabaseHelper extends SQLiteOpenHelper {
     private static LocalDatabaseHelper instance;
 
 
-    private static final int DATABASE_VERSION = 41;
+    private static final int DATABASE_VERSION = 42;
     public static final String DATABASE_NAME = "PITSTOP_DB";
 
     private BriteDatabase mBriteDatabase;
