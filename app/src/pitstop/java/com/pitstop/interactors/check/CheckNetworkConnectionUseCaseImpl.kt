370be3c99e2b3baf96ade1e2af58af4228a0feaa
Karol <karol@pitstopconnect.com>
2017-11-20 16:55:47 -0500

    added logs to "check" use cases

--- app/src/pitstop/java/com/pitstop/interactors/check/CheckNetworkConnectionUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/check/CheckNetworkConnectionUseCaseImpl.kt
@@ -1,6 +1,8 @@
 package com.pitstop.interactors.check
 
 import android.os.Handler
+import com.pitstop.models.DebugMessage
+import com.pitstop.utils.Logger
 import com.pitstop.utils.NetworkHelper
 
 /**
@@ -10,15 +12,19 @@ class CheckNetworkConnectionUseCaseImpl(val networkHelper: NetworkHelper
                                         , val useCaseHandler: Handler, val mainHandler: Handler) : CheckNetworkConnectionUseCase {
 
     private var callback: CheckNetworkConnectionUseCase.Callback? = null
-
+    private val tag = javaClass.simpleName
 
     override fun execute(callback: CheckNetworkConnectionUseCase.Callback) {
+        Logger.getInstance()!!.logE(tag, "Use case execution started", false, DebugMessage.TYPE_USE_CASE)
         this.callback = callback
         useCaseHandler.post(this)
     }
 
     override fun run() {
-        mainHandler.post({callback!!.onGotConnectionStatus(networkHelper.isConnected)})
+        val isConnected = networkHelper.isConnected
+        mainHandler.post({callback!!.onGotConnectionStatus(isConnected)})
+        Logger.getInstance()!!.logE(tag, "Use case finished: isConnected="+isConnected
+                , false, DebugMessage.TYPE_USE_CASE)
     }
 
 }
