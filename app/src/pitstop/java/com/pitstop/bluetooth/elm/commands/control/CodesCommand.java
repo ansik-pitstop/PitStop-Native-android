56e837a6c571bb99280245dba9f660e0e2990642
Karol <karol@pitstopconnect.com>
2018-01-22 09:48:15 -0500

    changed trouble code parsing logic and organized code to be more modular

--- app/src/pitstop/java/com/pitstop/bluetooth/elm/commands/control/CodesCommand.java
+++ app/src/pitstop/java/com/pitstop/bluetooth/elm/commands/control/CodesCommand.java
@@ -1,5 +1,7 @@
 package com.pitstop.bluetooth.elm.commands.control;
 
+import android.util.Log;
+
 import com.pitstop.bluetooth.elm.commands.ObdCommand;
 import com.pitstop.bluetooth.elm.enums.AvailableCommandNames;
 
@@ -52,35 +54,66 @@ public abstract class CodesCommand extends ObdCommand {
     /** {@inheritDoc} */
     @Override
     protected void performCalculations() {
-        final String result = getResult();
-        String workingData;
-        int startIndex = 0;//Header size.
 
-        String canOneFrame = result.replaceAll("[\r\n]", "");
-        int canOneFrameLength = canOneFrame.length();
-        if (canOneFrameLength <= 16 && canOneFrameLength % 4 == 0) {//CAN(ISO-15765) protocol one frame.
-            workingData = canOneFrame;//47yy{codes}
-            startIndex = 4;//Header is 47yy, yy showing the number of data items.
-        } else if (result.contains(":")) {//CAN(ISO-15765) protocol two and more frames.
-            workingData = result.replaceAll("[\r\n].:", "");//xxx47yy{codes}
-            startIndex = 7;//Header is xxx47yy, xxx is bytes of information to follow, yy showing the number of data items.
-        } else {//ISO9141-2, KWP2000 Fast and KWP2000 5Kbps (ISO15031) protocols.
-            workingData = result.replaceAll("^47|[\r\n]47|[\r\n]", "");
-        }
-        for (int begin = startIndex; begin < workingData.length(); begin += 4) {
+        for (String d: data){
             String dtc = "";
-            byte b1 = hexStringToByteArray(workingData.charAt(begin));
+            byte b1 = hexStringToByteArray(d.charAt(0));
             int ch1 = ((b1 & 0xC0) >> 6);
             int ch2 = ((b1 & 0x30) >> 4);
             dtc += dtcLetters[ch1];
             dtc += hexArray[ch2];
-            dtc += workingData.substring(begin+1, begin + 4);
+            dtc += d.substring(1,4);
             if (dtc.equals("P0000")) {
                 return;
             }
             codes.append(dtc);
             codes.append('\n');
         }
+    }
+
+    private boolean parseISO15765_CAN_ONE(String rawData){
+        Log.d(TAG,"praseISO15765_CAN_ONE() rawData: "+rawData);
+        int index = 0;
+        try{
+            while (index < rawData.length()){
+                if (hasHeaders()){
+                    headers.add(rawData.substring(index,index+getHeaderLen()));
+                    index += 2;
+                }
+                requestCode.add(rawData.substring(index,index+2));
+                index +=2;
+                int dtcCount = Integer.parseInt(rawData.substring(index,index+2),16);
+                codeCount.add(dtcCount);
+                index+=2;
+                System.out.println(TAG+": dtcCount: "+dtcCount);
+                for (int j=0;j<dtcCount;j++){
+                    data.add(rawData.substring(index,index+4));
+                    index+=4;
+                }
+            }
+        }catch(IndexOutOfBoundsException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private void parseISO15765_CAN_OTHER(String rawData){
+        Log.d(TAG,"praseISO15765_CAN_OTHER() rawData: "+rawData);
+        String workingData = rawData.replaceAll("[\r\n].:", "");  //xxx47yy{codes}
+        int startIndex = 7;//Header is xxx47yy, xxx is bytes of information to follow, yy showing the number of data items.
+
+        for (int begin = startIndex; begin < workingData.length(); begin += 4) {
+            data.add(workingData.substring(begin,begin+4));
+        }
+    }
+
+    private void parseOtherProtocol(String rawData){
+        Log.d(TAG,"praseOtherProtocol() rawData: "+rawData);
+        String workingData = rawData.replaceAll("^47|[\r\n]47|[\r\n]", "");
+        int startIndex = 0;
+
+
     }
 
     private byte hexStringToByteArray(char s) {
@@ -129,44 +162,23 @@ public abstract class CodesCommand extends ObdCommand {
             }
         }
 
-        rawData = res.toString().trim();
-        System.out.println(TAG+": rawData: "+rawData);
-
-        int startIndex = 0;
-        boolean dtcCountAvailable = true; //ISO-15765 returns dtc count in response
-        String workingData = rawData;
-        if (workingData.contains(":")) {//CAN(ISO-15765) protocol two and more frames.
-            workingData = workingData.replaceAll("[\r\n].:", "");//xxx47yy{codes}
-            startIndex = 3;//Header is xxx47yy, xxx is bytes of information to follow, yy showing the number of data items.
-        } else {//ISO9141-2, KWP2000 Fast and KWP2000 5Kbps (ISO15031) protocols.
-            workingData = workingData.replaceAll("^47|[\r\n]47|[\r\n]", "");
-        }
-
         /*
         * Store raw header, data and request code variables
         *
          */
-        int index = startIndex;
-        try{
-            while (index < rawData.length()){
-                if (hasHeaders()){
-                    headers.add(rawData.substring(index,index+getHeaderLen()));
-                    index += 2;
-                }
-                requestCode.add(rawData.substring(index,index+2));
-                index +=2;
-                int dtcCount = Integer.parseInt(rawData.substring(index,index+2),16);
-                codeCount.add(dtcCount);
-                index+=2;
-                System.out.println(TAG+": dtcCount: "+dtcCount);
-                for (int j=0;j<dtcCount;j++){
-                    data.add(rawData.substring(index,index+4));
-                    index+=4;
-                }
-            }
-        }catch(IndexOutOfBoundsException e){
-            e.printStackTrace();
+
+        rawData = res.toString().trim();
+        Log.d(TAG,"readRawData(): rawData: "+rawData+", length: "+rawData.length()+", headerLen: "
+                +getHeaderLen());
+        String workingData = rawData;
+        if ((workingData.length()-getHeaderLen()) <= 16 && (workingData.length()-getHeaderLen()) % 4 == 0) {//CAN(ISO-15765) protocol one frame.
+            parseISO15765_CAN_ONE(workingData);
+        } else if (workingData.contains(":")) {//CAN(ISO-15765) protocol two and more frames.
+            parseISO15765_CAN_OTHER(workingData);
+        } else {//ISO9141-2, KWP2000 Fast and KWP2000 5Kbps (ISO15031) protocols.
+            parseOtherProtocol(workingData);
         }
+
         System.out.printf(TAG+": After parsing Pending Trouble Codes, data: %s, header: %s" +
                 ", request code: %s",data.toString(),headers.toString(),requestCode.toString());
     }
