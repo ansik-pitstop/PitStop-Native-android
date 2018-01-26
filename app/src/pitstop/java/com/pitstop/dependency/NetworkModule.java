4f6842df123fccaf2c483d1dc62a2a6d70acbe67
Karol <karol@pitstopconnect.com>
2018-01-26 15:39:16 -0500

    401 unauthorized shouln't rippled down to use cases from the retrofit api

--- app/src/pitstop/java/com/pitstop/dependency/NetworkModule.java
+++ app/src/pitstop/java/com/pitstop/dependency/NetworkModule.java
@@ -2,9 +2,12 @@ package com.pitstop.dependency;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.util.Log;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.pitstop.application.GlobalApplication;
+import com.pitstop.retrofit.PitstopAuthApi;
 import com.pitstop.retrofit.PitstopCarApi;
 import com.pitstop.utils.NetworkHelper;
 import com.pitstop.utils.SecretUtils;
@@ -24,6 +27,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module (includes = {ContextModule.class, SharedPreferencesModule.class})
 public class NetworkModule {
+    private final String TAG = NetworkModule.class.getSimpleName();
 
     @Singleton
     @Provides
@@ -32,36 +36,79 @@ public class NetworkModule {
     }
 
     private OkHttpClient getHttpClientNoAuth(Context context){
-        GlobalApplication application = (GlobalApplication)context.getApplicationContext();
-        return new OkHttpClient.Builder()
-                .addInterceptor(chain -> {
-                    Request original = chain.request();
-                    Request.Builder builder = original.newBuilder()
-                            .header("client-id", SecretUtils.getClientId(context))
-                            .header("Content-Type", "application/json")
-                            .header("Authorization", "Bearer "+application);
-
-                    return chain.proceed(builder.build());
-                }).build();
-    }
-
-    private OkHttpClient getHttpClient(Context context){
-        GlobalApplication application = (GlobalApplication)context.getApplicationContext();
         return new OkHttpClient.Builder()
                 .addInterceptor(chain -> {
                     Request original = chain.request();
 
                     Request.Builder builder = original.newBuilder()
                             .header("client-id", SecretUtils.getClientId(context))
-                            .header("Content-Type", "application/json")
-                            .header("Authorization", "Bearer "+application.getAccessToken());
+                            .header("Content-Type", "application/json");
 
                     return chain.proceed(builder.build());
                 })
                 .build();
     }
 
+    private OkHttpClient getHttpClient(Context context){
+        GlobalApplication application = (GlobalApplication)context.getApplicationContext();
+        return new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Log.d(TAG,"Adding interceptor, access token: "+application.getAccessToken());
+                    Request original = chain.request();
+
+                    String prevAccessToken = application.getAccessToken();
+                    Request.Builder builder = original.newBuilder()
+                            .header("client-id", SecretUtils.getClientId(context))
+                            .header("Content-Type", "application/json")
+                            .header("Authorization", "Bearer "+prevAccessToken);
+
+                    okhttp3.Response response = null;
+                    Log.d(TAG,"creating real response");
+                    response = chain.proceed(builder.build());
+
+                    if (!response.isSuccessful() && response.code() == 401){
+                        while (prevAccessToken.equals(application.getAccessToken())){
+                            Log.d(TAG,"Token has not changed yet, sleeping 250ms");
+                            SystemClock.sleep(250);
+                            //wait until it changes
+                        }
+                        Log.d(TAG,"Token has changed, new token "+application.getAccessToken());
+                        //Make recursive call in half a second hoping other networking logic refreshed access token
+                        Request.Builder builderNew = original.newBuilder()
+                                    .header("client-id", SecretUtils.getClientId(context))
+                                    .header("Content-Type", "application/json")
+                                    .header("Authorization", "Bearer "+application.getAccessToken());
+                        return chain.proceed(builderNew.build());
+//                        JsonObject jsonObject = new JsonObject();
+//                        jsonObject.addProperty("refreshToken",application.getRefreshToken());
+//                        Response<Token> tokenResponse = pitstopAuthApi(context)
+//                                .refreshAccessToken(jsonObject).execute();
+//                        Log.d(TAG,"tokenResponse: "+tokenResponse);
+//                        if (tokenResponse.isSuccessful()){
+//                            String token = tokenResponse.body().getAccessToken();
+//                            Log.d(TAG,"received new token: "+token);
+//                            application.setTokens(token,application.getRefreshToken());
+//
+//                            //Update headers with new jwt token
+//                            Request.Builder builderNew = original.newBuilder()
+//                                    .header("client-id", SecretUtils.getClientId(context))
+//                                    .header("Content-Type", "application/json")
+//                                    .header("Authorization", "Bearer "+token);
+//                            return chain.proceed(builderNew.build()); //Ping same endpoint again after token has been refreshed
+//                        }else{
+//                            Log.d(TAG,"Token refresh request was not successful, raw response: "+tokenResponse.body());
+//                            return tokenResponse.raw(); //Return unsuccessful token refresh response //Todo: should log out here
+//                        }
+                    }else if (!response.isSuccessful()){
+                        Log.d(TAG,"Request failed for reason other than 401, err: "+response.message()+", code: "+response.code());
+                    }
+
+                    return response; //Return successful response & non 401 errors
+                }).build();
+    }
+
     @Provides
+    @Singleton
     public PitstopCarApi pitstopCarApi(Context context){
         return new Retrofit.Builder()
                 .baseUrl(SecretUtils.getEndpointUrl(context))
@@ -70,5 +117,17 @@ public class NetworkModule {
                 .client(getHttpClient(context))
                 .build()
                 .create(PitstopCarApi.class);
+    }
+
+    @Provides
+    @Singleton
+    PitstopAuthApi pitstopAuthApi(Context context){
+        return new Retrofit.Builder()
+                .baseUrl(SecretUtils.getEndpointUrl(context))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(getHttpClientNoAuth(context))
+                .build()
+                .create(PitstopAuthApi.class);
     }
 }
