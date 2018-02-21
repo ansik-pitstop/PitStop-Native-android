b0e8dba7ffcf0280a63b2b7e16501274c365c212
karolzdebel <karolmzdebel@gmail.com>
2018-02-21 15:36:56 +0100

    karol/ScanTabNoCarAdded (#936)
    * added romanian strings
    
    * Device search fragment & presenter now communicate what title should be displayed based on the state on the bluetooth connection observable
    
    * Device search fragment & presenter now communicate what title should be displayed based on the state on the bluetooth connection observable
    
    * storing strings from add car in strings.xml
    
    * Display progress along with title
    
    * If car isn't added change title, listen for updates about the addition of a car
    
    * Corrected race condition, request error is rippled down to the presenter
    
    * Further race condition handling

--- app/src/pitstop/java/com/pitstop/ui/vehicle_health_report/start_report/StartReportPresenter.java
+++ app/src/pitstop/java/com/pitstop/ui/vehicle_health_report/start_report/StartReportPresenter.java
@@ -2,8 +2,20 @@ package com.pitstop.ui.vehicle_health_report.start_report;
 
 import android.util.Log;
 
+import com.pitstop.EventBus.EventSource;
+import com.pitstop.EventBus.EventSourceImpl;
+import com.pitstop.EventBus.EventType;
+import com.pitstop.EventBus.EventTypeImpl;
+import com.pitstop.R;
 import com.pitstop.dependency.UseCaseComponent;
+import com.pitstop.interactors.get.GetUserCarUseCase;
+import com.pitstop.models.Car;
+import com.pitstop.models.Dealership;
+import com.pitstop.models.ReadyDevice;
+import com.pitstop.network.RequestError;
 import com.pitstop.observer.BluetoothConnectionObservable;
+import com.pitstop.observer.BluetoothConnectionObserver;
+import com.pitstop.ui.mainFragments.TabPresenter;
 import com.pitstop.utils.MixpanelHelper;
 
 
@@ -11,89 +23,263 @@ import com.pitstop.utils.MixpanelHelper;
  * Created by Matt on 2017-08-11.
  */
 
-public class StartReportPresenter {
+public class StartReportPresenter extends TabPresenter<StartReportView> implements BluetoothConnectionObserver {
+
+    public static final EventSource EVENT_SOURCE = new EventSourceImpl(EventSource.SOURCE_SCAN);
+
+    //Everything but car_id
+    public final EventType[] ignoredEvents = {
+            new EventTypeImpl(EventType.EVENT_MILEAGE)
+            , new EventTypeImpl(EventType.EVENT_CAR_DEALERSHIP)
+            , new EventTypeImpl(EventType.EVENT_DTC_NEW)
+            , new EventTypeImpl(EventType.EVENT_SCANNER)
+            , new EventTypeImpl(EventType.EVENT_SERVICES_HISTORY)
+            , new EventTypeImpl(EventType.EVENT_SERVICES_NEW)
+    };
 
     private final String TAG = StartReportPresenter.class.getSimpleName();
 
-    private StartReportView view;
     private MixpanelHelper mixpanelHelper;
     private UseCaseComponent useCaseComponent;
+    private BluetoothConnectionObservable bluetoothConnectionObservable;
+    private boolean carAdded = true; //Assume car is added, but check when loading view and set to false if not
 
-    public StartReportPresenter(UseCaseComponent useCaseComponent, MixpanelHelper mixpanelHelper) {
+    public StartReportPresenter(UseCaseComponent useCaseComponent
+            , MixpanelHelper mixpanelHelper) {
         this.useCaseComponent = useCaseComponent;
         this.mixpanelHelper = mixpanelHelper;
     }
 
-    public void subscribe(StartReportView view){
-        Log.d(TAG,"subscribe()");
-        this.view = view;
+    public void setBluetoothConnectionObservable(BluetoothConnectionObservable bluetoothConnectionObservable){
+        Log.d(TAG,"setBluetoothConnectionObservable() state: "+bluetoothConnectionObservable.getDeviceState());
+        this.bluetoothConnectionObservable = bluetoothConnectionObservable;
+        if (getView() != null){
+            bluetoothConnectionObservable.subscribe(this);
+
+            if (carAdded){
+                displayBluetoothState(bluetoothConnectionObservable);
+            }
+        }
     }
 
+    private void displayBluetoothState(BluetoothConnectionObservable bluetoothConnectionObservable){
+        String state = bluetoothConnectionObservable.getDeviceState();
+        switch(state){
+            case BluetoothConnectionObservable.State.CONNECTED_VERIFIED:
+                getView().changeTitle(R.string.device_connected_action_bar,false);
+                break;
+            case BluetoothConnectionObservable.State.CONNECTED_UNVERIFIED:
+                getView().changeTitle(R.string.verifying_device_action_bar,true);
+                break;
+            case BluetoothConnectionObservable.State.VERIFYING:
+                getView().changeTitle(R.string.verifying_device_action_bar,true);
+                break;
+            case BluetoothConnectionObservable.State.CONNECTING:
+                getView().changeTitle(R.string.connecting_to_device,true);
+                break;
+            case BluetoothConnectionObservable.State.FOUND_DEVICES:
+                getView().changeTitle(R.string.found_devices,true);
+                break;
+            case BluetoothConnectionObservable.State.SEARCHING:
+                getView().changeTitle(R.string.searching_for_device_action_bar,true);
+                break;
+            case BluetoothConnectionObservable.State.DISCONNECTED:
+                getView().changeTitle(R.string.scan_title_no_connection,false);
+                break;
+            default:
+                getView().changeTitle(R.string.device_not_connected_action_bar,false);
+                break;
+        }
+    }
+
+    @Override
+    public void subscribe(StartReportView view){
+        Log.d(TAG,"subscribe()");
+        super.subscribe(view);
+        carAdded = true;
+        if (bluetoothConnectionObservable != null){
+            bluetoothConnectionObservable.subscribe(this);
+        }
+    }
+
+    @Override
     public void unsubscribe(){
         Log.d(TAG,"unsubscribe()");
-        this.view = null;
+        super.unsubscribe();
+        carAdded = true;
+        if (bluetoothConnectionObservable != null){
+            bluetoothConnectionObservable.unsubscribe(this);
+        }
+    }
+
+    @Override
+    public EventType[] getIgnoredEventTypes() {
+        return ignoredEvents;
+    }
+
+    @Override
+    public void onAppStateChanged() {
+        Log.d(TAG,"onAppStateChanged() view null? "+(getView() == null));
+        if (getView() != null) loadView();
+    }
+
+    @Override
+    public EventSource getSourceType() {
+        return EVENT_SOURCE;
     }
 
     void onSwitchClicked(boolean b){
         Log.d(TAG,"onSwitchClicked()");
         mixpanelHelper.trackButtonTapped(MixpanelHelper.BUTTON_VHR_TOGGLE
                 ,MixpanelHelper.VIEW_VHR_TAB);
-        if(view == null){return;}
+        if(getView() == null){return;}
         if(b){
-            view.setModeEmissions();
+            getView().setModeEmissions();
         }else{
-            view.setModeHealthReport();
+            getView().setModeHealthReport();
         }
     }
 
     void onBluetoothSearchRequested(){
         Log.d(TAG,"onBluetoothSearchRequested()");
-        if (view == null) return;
-        view.getBluetoothConnectionObservable().requestDeviceSearch(true,false);
+        if (getView() == null) return;
+        getView().getBluetoothConnectionObservable().requestDeviceSearch(true,false);
     }
 
     void startReportButtonClicked(boolean emissions){
         Log.d(TAG,"startReportButtonClicked() emissions ? "+emissions);
         mixpanelHelper.trackButtonTapped(
                 MixpanelHelper.BUTTON_VHR_START,MixpanelHelper.VIEW_VHR_TAB);
-        if (view == null || view.getBluetoothConnectionObservable() == null) return;
+        if (getView() == null || getView().getBluetoothConnectionObservable() == null) return;
 
         //Check network connection
         useCaseComponent.getCheckNetworkConnectionUseCase().execute(status -> {
-            if (view == null) return;
-            else if (!status) view.displayOffline();
+            if (getView() == null) return;
+            else if (!status) getView().displayOffline();
+
+            //No car added
+            else if (!carAdded) getView().promptAddCar();
+
             //No bluetooth connection
-            else if (!view.getBluetoothConnectionObservable().getDeviceState()
+            else if (!getView().getBluetoothConnectionObservable().getDeviceState()
                     .equals(BluetoothConnectionObservable.State.CONNECTED_VERIFIED)){
 
                 //Ask for search
-                if (!view.getBluetoothConnectionObservable().getDeviceState()
+                if (!getView().getBluetoothConnectionObservable().getDeviceState()
                         .equals(BluetoothConnectionObservable.State.SEARCHING)){
-                    view.promptBluetoothSearch();
+                    getView().promptBluetoothSearch();
                 }else{
-                    view.displaySearchInProgress();
+                    getView().displaySearchInProgress();
                 }
 
             }
             else if (emissions){
-                view.startEmissionsProgressActivity();
+                getView().startEmissionsProgressActivity();
             }else{
-                view.startVehicleHealthReportProgressActivity();
+                getView().startVehicleHealthReportProgressActivity();
             }
         });
 
 
     }
 
+    void onAddCarClicked(){
+        Log.d(TAG,"onAddCarClicked()");
+        if (getView() != null) getView().startAddCar();
+    }
+
     void onShowReportsButtonClicked(boolean emissionMode){
         Log.d(TAG,"onShowReportsButtonClicked() emissionMode: "+emissionMode);
         mixpanelHelper.trackButtonTapped(
                 MixpanelHelper.BUTTON_VHR_PAST_REPORTS,MixpanelHelper.VIEW_VHR_TAB);
-        if (view == null) return;
-        if (emissionMode){
+        if (getView() == null)return;
+
+        if (!carAdded){
+            getView().promptAddCar();
+        }
+        else if (emissionMode){
             //Do nothing yet
         }else{
-            view.startPastReportsActivity();
+            getView().startPastReportsActivity();
         }
+    }
+
+    void onViewReadyForLoad(){
+        Log.d(TAG,"onViewReadyForLoad()");
+        if (getView() != null) loadView();
+    }
+
+    private void loadView(){
+        useCaseComponent.getUserCarUseCase().execute(new GetUserCarUseCase.Callback() {
+            @Override
+            public void onCarRetrieved(Car car, Dealership dealership, boolean isLocal) {
+                Log.d(TAG,"onCarRetrieved() car: "+car);
+                if (!isLocal){
+                    carAdded = true;
+                    if (bluetoothConnectionObservable != null)
+                        displayBluetoothState(bluetoothConnectionObservable);
+                }
+            }
+
+            @Override
+            public void onNoCarSet(boolean isLocal) {
+                if (!isLocal){
+                    Log.d(TAG,"onNoCarSet()");
+                    carAdded = false;
+                    if (getView() != null) getView().changeTitle(R.string.scan_title_add_car,false);
+                }
+            }
+
+            @Override
+            public void onError(RequestError error) {
+                Log.d(TAG,"onError() err: "+error);
+            }
+        });
+    }
+
+    @Override
+    public void onSearchingForDevice() {
+        Log.d(TAG,"onSearchingForDevice()");
+        if (carAdded && getView() != null) getView().changeTitle(R.string.searching_for_device_action_bar,true);
+    }
+
+    @Override
+    public void onDeviceReady(ReadyDevice readyDevice) {
+        Log.d(TAG,"onDeviceReady() readyDevice: "+readyDevice);
+        if (carAdded && getView() != null) getView().changeTitle(R.string.tap_to_begin,false);
+    }
+
+    @Override
+    public void onDeviceDisconnected() {
+        Log.d(TAG,"onDeviceDisconnected()");
+        if (carAdded && getView() != null) getView().changeTitle(R.string.scan_title_no_connection,false);
+    }
+
+    @Override
+    public void onDeviceVerifying() {
+        Log.d(TAG,"onDeviceVerifying");
+        if (carAdded && getView() != null) getView().changeTitle(R.string.verifying_device_action_bar,true);
+    }
+
+    @Override
+    public void onDeviceSyncing() {
+        Log.d(TAG,"onDeviceSyncing");
+    }
+
+    @Override
+    public void onGotSuportedPIDs(String value) {
+        Log.d(TAG,"onGotSupportedPIDs() value: "+value);
+    }
+
+    @Override
+    public void onConnectingToDevice() {
+        Log.d(TAG,"onConnectingToDevice()");
+        if (carAdded && getView() != null) getView().changeTitle(R.string.connecting_to_device,true);
+    }
+
+    @Override
+    public void onFoundDevices() {
+        Log.d(TAG,"onFoundDevices()");
+        if (carAdded && getView() != null) getView().changeTitle(R.string.found_devices,true);
     }
 }
