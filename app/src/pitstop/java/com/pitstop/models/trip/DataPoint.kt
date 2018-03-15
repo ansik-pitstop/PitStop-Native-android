f826f9a21f1b27229e8cdca2a24c94bd2242e54d
Karol <karol@pitstopconnect.com>
2018-03-15 13:12:25 +0100

    added models for storing trips

---
+++ app/src/pitstop/java/com/pitstop/models/trip/DataPoint.kt
@@ -0,0 +1,6 @@
+package com.pitstop.models.trip
+
+/**
+ * Created by Karol Zdebel on 3/15/2018.
+ */
+data class DataPoint(val id: String, val data: String)
---
+++ app/src/pitstop/java/com/pitstop/models/trip/TripData.kt
@@ -0,0 +1,6 @@
+package com.pitstop.models.trip
+
+/**
+ * Created by Karol Zdebel on 3/15/2018.
+ */
+data class TripData(val data: List<LocationDataPoint>)
