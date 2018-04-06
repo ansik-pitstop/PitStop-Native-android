df4d86c4d7c11ff289b01875963e298af5b87c2f
Karol <karol@pitstopconnect.com>
2018-04-06 14:28:14 +0200

    Smooch login use case executes every 60 seconds until it succeeds, handling socket timeout exception

--- app/src/pitstop/java/com/pitstop/interactors/other/SmoochLoginUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/other/SmoochLoginUseCaseImpl.kt
@@ -3,9 +3,11 @@ package com.pitstop.interactors.other
 import android.os.Handler
 import android.util.Log
 import com.pitstop.models.DebugMessage
+import com.pitstop.network.RequestError
 import com.pitstop.retrofit.PitstopSmoochApi
 import com.pitstop.utils.Logger
 import io.smooch.core.Smooch
+import java.net.SocketTimeoutException
 
 /**
  * Created by Karol Zdebel on 2/26/2018.
@@ -16,6 +18,7 @@ class SmoochLoginUseCaseImpl(val smoochApi: PitstopSmoochApi, val usecaseHandler
 
     private lateinit var callback: SmoochLoginUseCase.Callback
     private lateinit var userId: String
+    private val again = 60000L
 
     override fun execute(userId: String, callback: SmoochLoginUseCase.Callback) {
         Logger.getInstance()!!.logI(tag, "Use case execution started: userId=" + userId
@@ -28,35 +31,41 @@ class SmoochLoginUseCaseImpl(val smoochApi: PitstopSmoochApi, val usecaseHandler
 
     override fun run() {
         Log.d(tag,"run()")
-        val call = smoochApi.getSmoochToken(userId.toInt()).execute()
-        if (call.isSuccessful){
-            Log.d(tag,"call successful")
-            val body = call.body()
-            if (body != null){
-                val smoochToken = body.get("smoochToken").asString
-                Log.d(tag,"smooch token: "+smoochToken)
-                if (smoochToken != null){
-                    Smooch.login(userId,smoochToken, {
-                        Log.d(tag,"login response err: "+it.error)
-                        if (it.error == null) onLogin()
-                        else onError(it.error)
-                    })
+        try{
+            val call = smoochApi.getSmoochToken(userId.toInt()).execute()
+            if (call.isSuccessful){
+                Log.d(tag,"call successful")
+                val body = call.body()
+                if (body != null){
+                    val smoochToken = body.get("smoochToken").asString
+                    Log.d(tag,"smooch token: "+smoochToken)
+                    if (smoochToken != null){
+                        Smooch.login(userId,smoochToken, {
+                            Log.d(tag,"login response err: "+it.error)
+                            if (it.error == null) onLogin()
+                            else onError(it.error)
+                        })
+                    }else{
+                        Log.d(tag,"err smooch token null")
+                        onError("err smooch token null")
+                    }
                 }else{
-                    Log.d(tag,"err smooch token null")
-                    onError("err smooch token null")
+                    Log.d(tag,"err body null")
+                    onError("err body null")
                 }
             }else{
-                Log.d(tag,"err body null")
-                onError("err body null")
+                Log.d(tag,"call unsuccessful")
+                onError(call.message())
             }
-        }else{
-            Log.d(tag,"call unsuccessful")
-            onError(call.message())
+        }catch(e: SocketTimeoutException){
+            onError(RequestError.ERR_OFFLINE)
+            Handler().postDelayed(this,again)
         }
+
     }
 
     private fun onError(err: String){
-        Logger.getInstance()!!.logI(tag, "Use case returned error: err=" + err
+        Logger.getInstance()!!.logI(tag, "Use case returned error: err=$err, trying again in ${again/1000} seconds"
                 , DebugMessage.TYPE_USE_CASE)
         mainHandler.post({callback.onError(err)})
     }
