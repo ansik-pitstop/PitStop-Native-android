825c81f98202e56f663b3292f3d9d9ceb54f2d8b
Karol <karol@pitstopconnect.com>
2018-04-10 22:05:00 +0200

    added back button to notifications activity

--- app/src/pitstop/java/com/pitstop/ui/Notifications/NotificationsActivity.kt
+++ app/src/pitstop/java/com/pitstop/ui/Notifications/NotificationsActivity.kt
@@ -2,6 +2,8 @@ package com.pitstop.ui.Notifications
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
 import com.pitstop.R
 import com.pitstop.ui.main_activity.TabSwitcher
 
@@ -9,6 +11,8 @@ import com.pitstop.ui.main_activity.TabSwitcher
  * Created by Karol Zdebel on 4/10/2018.
  */
 class NotificationsActivity: AppCompatActivity(), TabSwitcher {
+
+    private val tag = javaClass.simpleName
 
     companion object{
         const val GO_TO_NONE = 0
@@ -23,6 +27,17 @@ class NotificationsActivity: AppCompatActivity(), TabSwitcher {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
         notificationFragment = NotificationFragment()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(tag, "onOptionsItemSelected()")
+        return if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onStart() {
@@ -33,16 +48,19 @@ class NotificationsActivity: AppCompatActivity(), TabSwitcher {
     }
 
     override fun openCurrentServices() {
+        Log.d(tag,"openCurrentServices()")
         setResult(GO_TO_SERVICES)
         finish()
     }
 
     override fun openAppointments() {
+        Log.d(tag,"openAppointments()")
         setResult(GO_TO_APPOINTMENTS)
         finish()
     }
 
     override fun openScanTab() {
+        Log.d(tag,"openScanTab()")
         setResult(GO_TO_SCAN)
         finish()
     }
