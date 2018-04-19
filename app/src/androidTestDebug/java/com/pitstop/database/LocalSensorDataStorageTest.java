038151fa1b07f1cca30129b3948627d70d8ec9a6
Karol <karol@pitstopconnect.com>
2018-04-19 13:44:04 +0200

    adding store() deleteAll() and getAll() tests

--- app/src/androidTestDebug/java/com/pitstop/database/LocalSensorDataStorageTest.java
+++ app/src/androidTestDebug/java/com/pitstop/database/LocalSensorDataStorageTest.java
@@ -1,8 +1,45 @@
 package com.pitstop.database;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Karol Zdebel on 4/19/2018.
  */
 
 public class LocalSensorDataStorageTest {
+    private final String TAG = LocalSensorDataStorageTest.class.getSimpleName();
+    private final String VIN = "1GB0CVCL7BF147611";
+
+    private LocalSensorDataStorage localSensorDataStorage;
+    private Gson gson;
+
+    @Before
+    public void setup() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        localSensorDataStorage = new LocalSensorDataStorage(context);
+        localSensorDataStorage.deleteAll();
+        gson = new Gson();
+    }
+
+    @Test
+    public void storeSensorDataTest(){
+        Log.d(TAG,"storeSensorDataTest()");
+    }
+
+    @Test
+    public void getAllSensorDataTest(){
+        Log.d(TAG,"getAllSensorDataTest");
+    }
+
+    @Test
+    public void deleteAllSensorDataTest(){
+        Log.d(TAG,"deleteSensorDataTest()");
+    }
 }
