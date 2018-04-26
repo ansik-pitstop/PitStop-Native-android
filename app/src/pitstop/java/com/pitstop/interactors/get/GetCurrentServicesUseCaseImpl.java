30da2090dc0324608fb05c3ed37645070440f67b
Karol <karol@pitstopconnect.com>
2018-04-26 13:07:31 +0200

    fixed bug which caused add car to sometimes show up even if car was present

--- app/src/pitstop/java/com/pitstop/interactors/get/GetCurrentServicesUseCaseImpl.java
+++ app/src/pitstop/java/com/pitstop/interactors/get/GetCurrentServicesUseCaseImpl.java
@@ -1,18 +1,23 @@
 package com.pitstop.interactors.get;
 
 import android.os.Handler;
+import android.util.Log;
 
 import com.pitstop.models.DebugMessage;
 import com.pitstop.models.Settings;
 import com.pitstop.models.issue.CarIssue;
 import com.pitstop.network.RequestError;
 import com.pitstop.repositories.CarIssueRepository;
+import com.pitstop.repositories.CarRepository;
 import com.pitstop.repositories.Repository;
 import com.pitstop.repositories.UserRepository;
 import com.pitstop.utils.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by Karol Zdebel on 5/31/2017.
@@ -24,17 +29,21 @@ public class GetCurrentServicesUseCaseImpl implements GetCurrentServicesUseCase 
 
     private UserRepository userRepository;
     private CarIssueRepository carIssueRepository;
+    private CarRepository carRepository;
     private Callback callback;
     private Handler useCaseHandler;
     private Handler mainHandler;
 
     public GetCurrentServicesUseCaseImpl(UserRepository userRepository
-            , CarIssueRepository carIssueRepository, Handler useCaseHandler, Handler mainHandler) {
+            , CarIssueRepository carIssueRepository
+            , CarRepository carRepository
+            , Handler useCaseHandler, Handler mainHandler) {
 
         this.useCaseHandler = useCaseHandler;
         this.mainHandler = mainHandler;
         this.userRepository = userRepository;
         this.carIssueRepository = carIssueRepository;
+        this.carRepository = carRepository;
     }
 
     @Override
@@ -73,12 +82,36 @@ public class GetCurrentServicesUseCaseImpl implements GetCurrentServicesUseCase 
 
                 if (!data.hasMainCar()){
                     //Check user car list
+                    carRepository.getCarsByUserId(data.getUserId())
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.from(useCaseHandler.getLooper()),true)
+                            .subscribe(next -> {
+                                //Ignore local responses
+                                if (next.isLocal()){}
+                                else if (next.getData() != null && next.getData().size() > 0){
+                                    getCurrentCarIssues(next.getData().get(0).getId());
 
-                    GetCurrentServicesUseCaseImpl.this.onNoCarAdded();
-                    return;
-                }
+                                    //Fix settings
+                                    userRepository.setUserCar(data.getUserId(), next.getData().get(0).getId()
+                                            , new Repository.Callback<Object>() {
 
-                getCurrentCarIssues(data.getCarId());
+                                                @Override
+                                                public void onSuccess(Object data) {
+                                                    Log.d(TAG,"fixed settings");
+                                                }
+
+                                                @Override
+                                                public void onError(RequestError error) {
+                                                    Log.d(TAG,"Error fixing settings");
+                                                }
+                                            });
+                                }else{
+                                    GetCurrentServicesUseCaseImpl.this.onNoCarAdded();
+                                }
+                            },error -> {
+                                GetCurrentServicesUseCaseImpl.this.onError(new RequestError(error));
+                            });
+                } else getCurrentCarIssues(data.getCarId());
             }
 
             @Override
