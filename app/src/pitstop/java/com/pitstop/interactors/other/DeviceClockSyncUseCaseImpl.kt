93461226544f9d16e6ea8da577fc19e6f89210ea
karolzdebel <karolmzdebel@gmail.com>
2018-05-14 14:01:48 +0200

    karol/DeviceClockSyncRobustness (#998)
    * made DeviceClockSyncUseCase more robust
    
    * implemented unit test for DeviceClockSyncUseCaseTest
    
    * use case is passing tests
    
    * reverting gradle versionName

--- app/src/pitstop/java/com/pitstop/interactors/other/DeviceClockSyncUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/other/DeviceClockSyncUseCaseImpl.kt
@@ -1,16 +1,24 @@
 package com.pitstop.interactors.other
 
 import android.os.Handler
+import android.util.Log
 import com.pitstop.models.DebugMessage
+import com.pitstop.models.Settings
 import com.pitstop.network.RequestError
+import com.pitstop.repositories.CarRepository
 import com.pitstop.repositories.Repository
 import com.pitstop.repositories.ScannerRepository
+import com.pitstop.repositories.UserRepository
 import com.pitstop.utils.Logger
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by Karol Zdebel on 11/3/2017.
  */
 class DeviceClockSyncUseCaseImpl(private val scannerRepository: ScannerRepository
+                                 , private val userRepository: UserRepository
+                                 , private val carRepository: CarRepository
                                  , private val useCaseHandler: Handler
                                  , private val mainHandler: Handler): DeviceClockSyncUseCase {
 
@@ -32,19 +40,84 @@ class DeviceClockSyncUseCaseImpl(private val scannerRepository: ScannerRepositor
     }
 
     override fun run() {
+        Log.d(tag,"run()")
+        //If the deviceId or VIN is missing then attempt to retrieve it from the car repository
+        if (deviceId.isEmpty() || vin.isEmpty()){
+            Log.d(tag,"device id is empty or vin is empty")
+            userRepository.getCurrentUserSettings(object: Repository.Callback<Settings>{
+                override fun onSuccess(data: Settings?) {
+                    if (data?.hasMainCar() == true){
+                        carRepository.get(data.carId)
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.from(useCaseHandler.looper),true)
+                                .subscribe({next ->
+                                    if (next.isLocal) return@subscribe //Only use remote responses
+                                    if (next.data == null){
+                                        Log.e(tag,"Data is null")
+                                        onErrorSyncingClock(RequestError.getUnknownError())
+                                    }else{
+                                        Log.d(tag,"car scanner id: ${next.data.scannerId}, device id in execute: $deviceId")
+                                        //Use deviceId directly from device since its not associated with car on backend
+                                        if (!deviceId.isEmpty() && next.data.scannerId.isNullOrEmpty()){
+                                            Log.d(tag,"using deviceId: $deviceId, vin: ${next.data.vin}")
+                                            deviceClockSync(rtcTime, deviceId, next.data.vin)
+                                        }
+                                        //Use deviceId and VIN both which are associated with the currently selected car by user
+                                        else if (next.data.scannerId != null){
+                                            Log.d(tag,"using deviceId: ${next.data.scannerId}, vin: ${next.data.vin}")
+                                            deviceClockSync(rtcTime, next.data.scannerId, next.data.vin)
+                                        }else{
+                                            Log.e(tag,"no device id to associate with vehicle")
+                                            onErrorSyncingClock(RequestError.getUnknownError())
+                                        }
+                                    }
+                                },{err ->
+                                    onErrorSyncingClock(RequestError(err))
+                                })
+
+                    }else{
+                        Log.e(tag,"No main car found")
+                        onErrorSyncingClock(RequestError.getUnknownError())
+                    }
+                }
+
+                override fun onError(error: RequestError?) {
+                    Log.e(tag,"error retrieving settings")
+                    onErrorSyncingClock(error ?: RequestError.getUnknownError())
+                }
+
+            })
+        }
+        //Otherwise use values provided directly by the device
+        else{
+            Log.d(tag,"using deviceId: $deviceId, vin: $vin")
+            deviceClockSync(rtcTime,deviceId,vin)
+        }
+    }
+
+    private fun onClockSynced(){
+        Logger.getInstance().logI(tag, "Use case finished: success"
+                , DebugMessage.TYPE_USE_CASE)
+        mainHandler.post({callback!!.onClockSynced()})
+    }
+
+    private fun onErrorSyncingClock(error: RequestError){
+        Logger.getInstance().logE(tag, "Use case returned error: error=$error"
+                , DebugMessage.TYPE_USE_CASE)
+        mainHandler.post({callback!!.onError(error)})
+    }
+
+    private fun deviceClockSync(rtcTime: Long, deviceId: String, vin: String){
         scannerRepository.deviceClockSync(rtcTime,deviceId,vin.toUpperCase(),"scanner"
                 , object: Repository.Callback<String>{
 
             override fun onSuccess(data: String) {
-                Logger.getInstance().logI(tag, "Use case finished: success"
-                        , DebugMessage.TYPE_USE_CASE)
-                mainHandler.post({callback!!.onClockSynced()})
+                onClockSynced()
             }
 
             override fun onError(error: RequestError) {
-                Logger.getInstance().logE(tag, "Use case returned error: error=$error"
-                        , DebugMessage.TYPE_USE_CASE)
-                mainHandler.post({callback!!.onError(error)})
+                Log.e(tag,"error syncing clock")
+                onErrorSyncingClock(error)
             }
 
         })
