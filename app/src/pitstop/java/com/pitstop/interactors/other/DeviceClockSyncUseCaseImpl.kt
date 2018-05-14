2c9dfb43005be2a6ed4e13c97b2d5a0768514d02
Karol <karol@pitstopconnect.com>
2018-05-14 11:20:26 +0200

    made DeviceClockSyncUseCase more robust

--- app/src/pitstop/java/com/pitstop/interactors/other/DeviceClockSyncUseCaseImpl.kt
+++ app/src/pitstop/java/com/pitstop/interactors/other/DeviceClockSyncUseCaseImpl.kt
@@ -2,15 +2,22 @@ package com.pitstop.interactors.other
 
 import android.os.Handler
 import com.pitstop.models.DebugMessage
+import com.pitstop.models.Settings
 import com.pitstop.network.RequestError
+import com.pitstop.repositories.CarRepository
 import com.pitstop.repositories.Repository
 import com.pitstop.repositories.ScannerRepository
+import com.pitstop.repositories.UserRepository
 import com.pitstop.utils.Logger
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by Karol Zdebel on 11/3/2017.
  */
 class DeviceClockSyncUseCaseImpl(private val scannerRepository: ScannerRepository
+                                 , private val userRepository: UserRepository
+                                 , private val carRepository: CarRepository
                                  , private val useCaseHandler: Handler
                                  , private val mainHandler: Handler): DeviceClockSyncUseCase {
 
@@ -32,19 +39,69 @@ class DeviceClockSyncUseCaseImpl(private val scannerRepository: ScannerRepositor
     }
 
     override fun run() {
+
+        //If the deviceId or VIN is missing then attempt to retrieve it from the car repository
+        if (deviceId.isEmpty() || vin.isEmpty()){
+            userRepository.getCurrentUserSettings(object: Repository.Callback<Settings>{
+                override fun onSuccess(data: Settings?) {
+                    if (data?.hasMainCar() == true){
+                        carRepository.get(data.carId)
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.from(useCaseHandler.looper),true)
+                                .subscribe({next ->
+                                    if (next.data == null){
+                                        onErrorSyncingClock(RequestError.getUnknownError())
+                                    }else{
+                                        //Use deviceId directly from device since its not associated with car on backend
+                                        if (!deviceId.isEmpty() && next.data.scannerId.isEmpty()){
+                                            deviceClockSync(rtcTime, deviceId, next.data.vin)
+                                        }
+                                        //Use deviceId and VIN both which are associated with the currently selected car by user
+                                        else{
+                                            deviceClockSync(rtcTime, next.data.scannerId, next.data.vin)
+                                        }
+                                    }
+                                },{err ->
+                                    onErrorSyncingClock(RequestError(err))
+                                })
+
+                    }else{
+                        onErrorSyncingClock(RequestError.getUnknownError())
+                    }
+                }
+
+                override fun onError(error: RequestError?) {
+                    onErrorSyncingClock(error ?: RequestError.getUnknownError())
+                }
+
+            })
+        }
+        //Otherwise use values provided directly by the device
+        else deviceClockSync(rtcTime,deviceId,vin)
+    }
+
+    private fun onClockSynced(){
+        Logger.getInstance().logI(tag, "Use case finished: success"
+                , DebugMessage.TYPE_USE_CASE)
+        mainHandler.post({callback!!.onClockSynced()})
+    }
+
+    private fun onErrorSyncingClock(error: RequestError){
+        Logger.getInstance().logE(tag, "Use case returned error: error=$error"
+                , DebugMessage.TYPE_USE_CASE)
+        mainHandler.post({callback!!.onError(error)})
+    }
+
+    private fun deviceClockSync(rtcTime: Long, deviceId: String, vin: String){
         scannerRepository.deviceClockSync(rtcTime,deviceId,vin.toUpperCase(),"scanner"
                 , object: Repository.Callback<String>{
 
             override fun onSuccess(data: String) {
-                Logger.getInstance().logI(tag, "Use case finished: success"
-                        , DebugMessage.TYPE_USE_CASE)
-                mainHandler.post({callback!!.onClockSynced()})
+                onClockSynced()
             }
 
             override fun onError(error: RequestError) {
-                Logger.getInstance().logE(tag, "Use case returned error: error=$error"
-                        , DebugMessage.TYPE_USE_CASE)
-                mainHandler.post({callback!!.onError(error)})
+                onErrorSyncingClock(error)
             }
 
         })
