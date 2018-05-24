a9300d63750ac8a02a33bf25fbadc01978c9ae79
Karol <karol@pitstopconnect.com>
2018-05-24 18:49:28 +0200

    created mileage dialog class responsibile for handling interactions with the layout

---
+++ app/src/pitstop/java/com/pitstop/ui/dialog/MileageDialog.kt
@@ -0,0 +1,12 @@
+package com.pitstop.ui.dialog
+
+import android.app.Dialog
+import android.content.Context
+
+/**
+ * Created by Karol Zdebel on 5/24/2018.
+ */
+class MileageDialog(context: Context): Dialog(context) {
+
+
+}
