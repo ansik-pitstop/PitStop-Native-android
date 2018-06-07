940e7a2e75b54a09abb19f9542bdbdb85097140e
Karol <karol@pitstopconnect.com>
2018-06-07 20:40:35 +0200

    removed trip settings fragment, a lot of code from trips service and dependencies on those modules

--- app/src/pitstop/java/com/pitstop/ui/trip/TripBroadcastReceiver.kt
+++ app/src/pitstop/java/com/pitstop/ui/trip/TripBroadcastReceiver.kt
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.google.android.gms.location.DetectedActivity
+import com.google.android.gms.location.ActivityRecognitionResult
 import com.google.android.gms.location.LocationResult
 import com.pitstop.database.LocalActivityStorage
 import com.pitstop.database.LocalCarStorage
@@ -23,85 +23,76 @@ class TripBroadcastReceiver: BroadcastReceiver() {
     private val tag = TripBroadcastReceiver::class.java.simpleName
 
     companion object {
-        const val DETECTED_ACTIVITY = "detected_activity"
         const val GOT_LOCATION = "got_location"
-        const val ACTIVITY_EXTRA = "activity_extra"
         const val LOCATION_EXTRA = "location_extra"
         const val ACTION_PROCESS_UPDATE = "action_process_update"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(tag,"onReceive()")
-        var still = false
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            val activityResult = ActivityRecognitionResult.extractResult(intent)
+            Logger.getInstance().logD(tag,"Received activity recognition intent",DebugMessage.TYPE_TRIP)
+
+            val localUserStorage = LocalUserStorage(context)
+            if (localUserStorage.user == null || localUserStorage.user.id == -1){
+                if (localUserStorage.user == null || localUserStorage.user.id == -1){
+                    Logger.getInstance().logE(tag,"User id is null!",DebugMessage.TYPE_TRIP)
+                    return
+                }
+            }
+            val carId = localUserStorage.user.settings.carId
+            val localCarStorage = LocalCarStorage(context)
+            val vin = localCarStorage.getCar(carId)?.vin
+            val localActivityStorage = LocalActivityStorage(context)
+            val carActivity = arrayListOf<CarActivity>()
+
+            if (vin == null){
+                Logger.getInstance().logE(tag,"Vin is null! ",DebugMessage.TYPE_TRIP)
+            }
+
+
+            activityResult.probableActivities.forEach({
+                carActivity.add(CarActivity(vin ?: "",System.currentTimeMillis(),it.type,it.confidence))
+            })
+
+            val rows = localActivityStorage.store(carActivity)
+            Logger.getInstance().logD(tag,"Stored activities locally, response: $rows"
+                    , DebugMessage.TYPE_TRIP)
+        }
         if (LocationResult.hasResult(intent)){
             val locationResult = LocationResult.extractResult(intent)
             val intent = Intent(TripBroadcastReceiver.GOT_LOCATION)
             intent.putExtra(LOCATION_EXTRA,locationResult)
+
             Logger.getInstance().logD(tag,"Received activity location intent, loc.size = " +
                     "${locationResult.locations.size}", DebugMessage.TYPE_TRIP)
-            //sendBroadcast(intent)
 
             Log.d(tag,"received locations: ${locationResult.locations}")
 
             val localUserStorage = LocalUserStorage(context)
-            if (localUserStorage.user == null || localUserStorage.user.id == -1) return
+            if (localUserStorage.user == null || localUserStorage.user.id == -1){
+                Logger.getInstance().logE(tag,"User id is null!",DebugMessage.TYPE_TRIP)
+                return
+            }
             val carId = localUserStorage.user.settings.carId
             val localCarStorage = LocalCarStorage(context)
             val vin = localCarStorage.getCar(carId)?.vin
             val localLocationStorage = LocalLocationStorage(context)
             val locations = arrayListOf<CarLocation>()
-            Log.d(tag,"got vin: $vin")
+
+            if (vin == null){
+                Logger.getInstance().logE(tag,"Vin is null!",DebugMessage.TYPE_TRIP)
+            }
+
             locationResult.locations.forEach({
                 locations.add(CarLocation(vin ?: "",System.currentTimeMillis(),it.longitude,it.latitude))
             })
-            val lastLocalLoc = localLocationStorage.getAll().lastOrNull()
-            Log.d(tag,"last loc: $lastLocalLoc, locations received: ${locations[0]}")
-            if (lastLocalLoc != null && lastLocalLoc.latitude == locations[0].latitude && lastLocalLoc.longitude == locations[0].longitude){
-                still = true
-            }
+
             val rows = localLocationStorage.store(locations)
             Logger.getInstance().logD(tag,"Stored locations locally, response: $rows"
                     , DebugMessage.TYPE_TRIP)
-        }else{
-            Log.d(tag,"location unavailable")
         }
-
-        val detectedActivities = arrayListOf<DetectedActivity>()
-        Log.d(tag,"still=$still")
-        if (still){
-
-            detectedActivities.add(DetectedActivity(DetectedActivity.IN_VEHICLE,10))
-            detectedActivities.add(DetectedActivity(DetectedActivity.ON_FOOT,95))
-            detectedActivities.add(DetectedActivity(DetectedActivity.STILL,10))
-            detectedActivities.add(DetectedActivity(DetectedActivity.WALKING,95))
-
-
-        }else{
-            detectedActivities.add(DetectedActivity(DetectedActivity.IN_VEHICLE,80))
-            detectedActivities.add(DetectedActivity(DetectedActivity.ON_FOOT,20))
-            detectedActivities.add(DetectedActivity(DetectedActivity.STILL,10))
-            detectedActivities.add(DetectedActivity(DetectedActivity.WALKING,20))
-
-        }
-
-        val localUserStorage = LocalUserStorage(context)
-        val carId = localUserStorage.user.settings.carId
-        val localCarStorage = LocalCarStorage(context)
-        if (localUserStorage.user == null || localUserStorage.user.id == -1) return
-        val vin = localCarStorage.getCar(carId)?.vin
-        val localActivityStorage = LocalActivityStorage(context)
-        val carActivity = arrayListOf<CarActivity>()
-//            activityResult.probableActivities.forEach({
-//                carActivity.add(CarActivity(vin ?: "",System.currentTimeMillis(),it.type,it.confidence))
-//            })
-
-        detectedActivities.forEach({
-            carActivity.add(CarActivity(vin ?: "",System.currentTimeMillis(),it.type,it.confidence))
-        })
-        Log.d(tag,"got vin: $vin")
-        val rows = localActivityStorage.store(carActivity)
-        Logger.getInstance().logD(tag,"Stored detected activities locally, response: $rows"
-                , DebugMessage.TYPE_TRIP)
 
     }
 
