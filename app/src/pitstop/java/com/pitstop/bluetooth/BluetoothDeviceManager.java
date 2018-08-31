11d98d12c4bb3fb1f3a5db1d7be09e5f55e97a84
Karol <karol@pitstopconnect.com>
2018-08-31 15:12:53 -0400

    moved bluetooth search and connect logic to RegularBluetoothDeviceSearcher from BluetoothDeviceManager

--- app/src/pitstop/java/com/pitstop/bluetooth/BluetoothDeviceManager.java
+++ app/src/pitstop/java/com/pitstop/bluetooth/BluetoothDeviceManager.java
@@ -1,13 +1,7 @@
 package com.pitstop.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Handler;
 import android.util.Log;
 
 import com.pitstop.application.GlobalApplication;
@@ -21,15 +15,12 @@ import com.pitstop.bluetooth.communicator.ObdManager;
 import com.pitstop.bluetooth.dataPackages.DtcPackage;
 import com.pitstop.bluetooth.dataPackages.PidPackage;
 import com.pitstop.bluetooth.elm.enums.ObdProtocols;
+import com.pitstop.bluetooth.searcher.RegularBluetoothDeviceSearcher;
 import com.pitstop.dependency.ContextModule;
 import com.pitstop.dependency.DaggerUseCaseComponent;
 import com.pitstop.dependency.UseCaseComponent;
 import com.pitstop.models.DebugMessage;
 import com.pitstop.utils.Logger;
-import com.pitstop.utils.MixpanelHelper;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Ben!
@@ -40,19 +31,34 @@ public class BluetoothDeviceManager{
 
     private Context mContext;
     private GlobalApplication application;
-    private MixpanelHelper mixpanelHelper;
     private ObdManager.IBluetoothDataListener dataListener;
-
-    private BluetoothAdapter mBluetoothAdapter;
-    private Handler mHandler = new Handler();
+    private RegularBluetoothDeviceSearcher regularBluetoothDeviceSearcher;
 
     private AbstractDevice deviceInterface;
     private UseCaseComponent useCaseComponent;
 
-    private int discoveryNum = 0;
     private int btConnectionState = BluetoothCommunicator.DISCONNECTED;
-    private boolean nonUrgentScanInProgress = false;
-    private boolean discoveryWasStarted = false;
+
+    public enum CommType {
+        CLASSIC, LE
+    }
+
+    public enum DeviceType {
+        ELM327, OBD215, OBD212
+    }
+
+    public BluetoothDeviceManager(Context context) {
+
+        mContext = context;
+        application = (GlobalApplication) context.getApplicationContext();
+
+        useCaseComponent = DaggerUseCaseComponent.builder()
+                .contextModule(new ContextModule(mContext))
+                .build();
+
+        regularBluetoothDeviceSearcher
+                = new RegularBluetoothDeviceSearcher(useCaseComponent,dataListener,context,this);
+    }
 
     public void setState(int state) {
         Log.d(TAG,"setState() state: "+state);
@@ -82,87 +88,22 @@ public class BluetoothDeviceManager{
         dataListener.onGotRtc(l);
     }
 
-    public enum CommType {
-        CLASSIC, LE
-    }
-
-    public enum DeviceType {
-        ELM327, OBD215, OBD212
-    }
-
-    public BluetoothDeviceManager(Context context) {
-
-        mContext = context;
-        application = (GlobalApplication) context.getApplicationContext();
-        mixpanelHelper = new MixpanelHelper(application);
-
-        useCaseComponent = DaggerUseCaseComponent.builder()
-                .contextModule(new ContextModule(mContext))
-                .build();
-
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) mContext.getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-
-        // for classic discovery
-        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        context.registerReceiver(receiver, intentFilter);
-
-    }
-
     public void setBluetoothDataListener(ObdManager.IBluetoothDataListener dataListener) {
         this.dataListener = dataListener;
     }
 
-    //Returns false if search didn't begin again
-    private boolean ignoreVerification = false;
-
     public synchronized boolean startScan(boolean urgent, boolean ignoreVerification) {
         Log.d(TAG, "startScan() urgent: " + Boolean.toString(urgent) + " ignoreVerification: " + Boolean.toString(ignoreVerification));
-        this.ignoreVerification = ignoreVerification;
-        if (!mBluetoothAdapter.isEnabled() && !urgent) {
-            Log.i(TAG, "Scan unable to start, bluetooth is disabled and non urgent scan");
-            return false;
-        }
-        else if (!mBluetoothAdapter.isEnabled() && urgent){
-            Log.d(TAG, "urgent scan, bluetooth is enabled()");
-            mBluetoothAdapter.enable(); //Enable bluetooth, scan will be triggered someplace else
-            return false;
-        }
-
-        if (mBluetoothAdapter.isDiscovering()) {
-            Log.i(TAG, "Already scanning");
-            return false;
-        }
-
-        return connectBluetooth(urgent);
+        return regularBluetoothDeviceSearcher.startScan(urgent,ignoreVerification);
     }
 
     public synchronized void onConnectDeviceValid(){
-        if (mBluetoothAdapter.isEnabled() && mBluetoothAdapter.isDiscovering()){
-            Log.i(TAG,"Stopping scan");
-            mBluetoothAdapter.cancelDiscovery();
-            dataListener.scanFinished();
-        }
+        regularBluetoothDeviceSearcher.onConnectDeviceValid();
     }
 
     public void close() {
         Log.d(TAG,"close()");
-        btConnectionState = IBluetoothCommunicator.DISCONNECTED;
-        if (mBluetoothAdapter != null && mBluetoothAdapter.isDiscovering()){
-            mBluetoothAdapter.cancelDiscovery();
-            dataListener.scanFinished();
-        }
-
-        if (deviceInterface != null ) {
-            deviceInterface.closeConnection();
-        }
-        try {
-            mContext.unregisterReceiver(receiver);
-        } catch (Exception e) {
-            Log.d(TAG, "Receiver not registered");
-        }
+        regularBluetoothDeviceSearcher.close();
     }
 
 
@@ -184,202 +125,9 @@ public class BluetoothDeviceManager{
     }
 
     public void changeScanUrgency(boolean urgent){
-        this.nonUrgentScanInProgress = !urgent;
+        regularBluetoothDeviceSearcher.changeScanUrgency(urgent);
     }
 
-    public int getState() {
-        return btConnectionState;
-    }
-
-    private synchronized boolean connectBluetooth(boolean urgent) {
-        nonUrgentScanInProgress = !urgent; //Set the flag regardless of whether a scan is in progress
-        btConnectionState = deviceInterface == null ? BluetoothCommunicator.DISCONNECTED : deviceInterface.getCommunicatorState();
-
-        if (btConnectionState == BluetoothCommunicator.CONNECTED) {
-            Log.i(TAG, "Bluetooth connected");
-            return false;
-        }
-
-        if (btConnectionState == BluetoothCommunicator.CONNECTING) {
-            Log.i(TAG, "Bluetooth already connecting");
-            return false;
-        }
-
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Log.i(TAG, "Bluetooth not enabled or BluetoothAdapt is null");
-            return false;
-        }
-
-        //Order matters in the IF condition below, if rssiScan=true then discovery will not be started
-        if (!rssiScan && mBluetoothAdapter.startDiscovery()){
-            Logger.getInstance().logI(TAG,"Discovery started", DebugMessage.TYPE_BLUETOOTH);
-            //If discovery takes longer than 20 seconds, timeout and cancel it
-            discoveryWasStarted = true;
-            discoveryNum++;
-            useCaseComponent.discoveryTimeoutUseCase().execute(discoveryNum, timerDiscoveryNum -> {
-                if (discoveryNum == timerDiscoveryNum
-                        && discoveryWasStarted){
-                    Logger.getInstance().logE(TAG,"Discovery timeout", DebugMessage.TYPE_BLUETOOTH);
-                    mBluetoothAdapter.cancelDiscovery();
-                }
-            });
-
-            rssiScan = true;
-            foundDevices.clear(); //Reset found devices map from previous scan
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    private void connectTo212Device(BluetoothDevice device){
-        Log.d(TAG,"connectTo212Device() device: "+device.getName());
-        deviceInterface = new Device212B(mContext, dataListener, device.getName(), this);
-
-        deviceInterface.connectToDevice(device);
-    }
-
-    private void connectTo215Device(BluetoothDevice device) {
-        Log.d(TAG,"connectTo215Device() device: "+device.getName());
-        deviceInterface = new Device215B(mContext, dataListener
-                , device.getName(), BluetoothDeviceManager.this);
-
-        deviceInterface.connectToDevice(device);
-
-    }
-
-    private void connectToELMDevice(BluetoothDevice device){
-        Log.d(TAG,"connectToELM327Device() device: "+device.getName());
-        deviceInterface = new ELM327Device( mContext, this);
-        dataListener.setDeviceName(device.getAddress());
-        deviceInterface.connectToDevice(device);
-    }
-
-    public boolean moreDevicesLeft(){
-        return foundDevices.size() > 0;
-    }
-
-    //Returns whether a device qualified for connection
-    public synchronized boolean connectToNextDevice(){
-        Log.d(TAG,"connectToNextDevice(), foundDevices count: "+foundDevices.keySet().size());
-
-        short minRssiThreshold;
-        short strongestRssi = Short.MIN_VALUE;
-        BluetoothDevice strongestRssiDevice = null;
-
-        if (nonUrgentScanInProgress){
-            minRssiThreshold = -70;
-
-        }
-        //Deliberate scan, connect regardless
-        else{
-            minRssiThreshold = Short.MIN_VALUE;
-        }
-
-        Log.d(TAG,"minRssiThreshold set to: "+minRssiThreshold);
-
-        for (Map.Entry<BluetoothDevice,Short> device: foundDevices.entrySet()){
-            if (device.getValue() != null && device.getValue() > strongestRssi){
-                strongestRssiDevice = device.getKey();
-                strongestRssi = device.getValue();
-            }
-        }
-        Log.d(TAG,"strongest rssi: "+strongestRssi);
-        if (strongestRssiDevice == null || strongestRssi < minRssiThreshold) {
-            Log.d(TAG,"No device was found as candidate for a potential connection.");
-            foundDevices.clear();
-            return false;
-
-        }
-
-        //Close previous connection
-        if (deviceInterface != null){
-            deviceInterface.closeConnection();
-        }
-
-        if (strongestRssiDevice.getName().contains(ObdManager.BT_DEVICE_NAME_212)) {
-            Log.d(TAG, "device212 > RSSI_Threshold, device: " + strongestRssiDevice);
-
-            foundDevices.remove(strongestRssiDevice);
-            connectTo212Device(strongestRssiDevice);
-        } else if (strongestRssiDevice.getName().contains(ObdManager.BT_DEVICE_NAME_215)) {
-            Log.d(TAG, "device215 > RSSI_Threshold, device: " + strongestRssiDevice);
-            foundDevices.remove(strongestRssiDevice);
-            connectTo215Device(strongestRssiDevice);
-        }
-        else if (strongestRssiDevice.getName().contains(ObdManager.CARISTA_DEVICE)||
-                strongestRssiDevice.getName().contains(ObdManager.VIECAR_DEVICE)||
-                strongestRssiDevice.getName().contains(ObdManager.OBDII_DEVICE_NAME) ||
-                strongestRssiDevice.getName().contains(ObdManager.OBD_LINK_MX)
-                ){
-            Log.d(TAG, "CaristaDevice> RSSI_Threshold, device: " + strongestRssiDevice);
-            foundDevices.remove(strongestRssiDevice);
-            connectToELMDevice(strongestRssiDevice);
-        }
-
-        return true;
-    }
-
-    private Map<BluetoothDevice, Short> foundDevices = new HashMap<>(); //Devices found by receiver
-    private boolean rssiScan = false;
-
-    // for classic discovery
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            //Found device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                Log.d(TAG, "name: "+device.getName() + ", address: " + device.getAddress()+" RSSI: "+rssi);
-                //Store all devices in a map
-                if (device.getName() != null && (device.getName().contains(ObdManager.BT_DEVICE_NAME) ||
-                        device.getName().equalsIgnoreCase(ObdManager.CARISTA_DEVICE)
-                        ||device.getName().equalsIgnoreCase(ObdManager.VIECAR_DEVICE)
-                        || device.getName().equalsIgnoreCase(ObdManager.OBDII_DEVICE_NAME)
-                        || device.getName().equalsIgnoreCase(ObdManager.OBD_LINK_MX))
-                        && !foundDevices.containsKey(device)){
-                    foundDevices.put(device,rssi);
-                    Log.d(TAG,"foundDevices.put() device name: "+device.getName());
-                }
-                else{
-                    Log.d(TAG,"Device did not meet criteria for foundDevice list name="+device.getName());
-                }
-            }
-            //Finished scanning
-            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-                discoveryWasStarted = false;
-                discoveryNum++;
-                Logger.getInstance().logI(TAG,"Discovery finished", DebugMessage.TYPE_BLUETOOTH);
-
-                //Connect to device with strongest signal if scan has been requested
-                if (rssiScan){
-                    rssiScan = false;
-                    dataListener.onDevicesFound();
-                    String foundDevicesString = "{";
-                    for (Map.Entry<BluetoothDevice,Short> d: foundDevices.entrySet()){
-                        foundDevicesString += d.getKey().getName()+"="+d.getValue()+",";
-                    }
-                    foundDevicesString+="}";
-
-                    Logger.getInstance().logI(TAG,"Found devices: "+foundDevicesString, DebugMessage.TYPE_BLUETOOTH);
-                    mixpanelHelper.trackFoundDevices(foundDevices);
-                    if (foundDevices.size() > 0){
-                        //Try to connect to available device, if none qualify then finish scan
-                        if (!connectToNextDevice()) dataListener.scanFinished();
-                    }else{
-                        //Notify scan finished after 0.5 seconds due to delay
-                        // in receiving CONNECTING notification
-                        dataListener.scanFinished();
-                    }
-                }
-
-            }
-        }
-    };
     public void getVin() {
         Log.d(TAG,"getVin() btConnectionState: "+btConnectionState);
         if (btConnectionState != BluetoothCommunicator.CONNECTED) {
--- app/src/pitstop/java/com/pitstop/bluetooth/searcher/RegularBluetoothDeviceSearcher.kt
+++ app/src/pitstop/java/com/pitstop/bluetooth/searcher/RegularBluetoothDeviceSearcher.kt
@@ -2,9 +2,20 @@ package com.pitstop.bluetooth.searcher
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.util.Log
+import com.pitstop.bluetooth.BluetoothDeviceManager
 import com.pitstop.bluetooth.bleDevice.AbstractDevice
+import com.pitstop.bluetooth.bleDevice.Device212B
+import com.pitstop.bluetooth.bleDevice.Device215B
+import com.pitstop.bluetooth.bleDevice.ELM327Device
 import com.pitstop.bluetooth.communicator.BluetoothCommunicator
+import com.pitstop.bluetooth.communicator.IBluetoothCommunicator
+import com.pitstop.bluetooth.communicator.ObdManager
 import com.pitstop.dependency.UseCaseComponent
 import com.pitstop.models.DebugMessage
 import com.pitstop.utils.Logger
@@ -13,11 +24,14 @@ import java.util.*
 /**
  * Created by Karol Zdebel on 8/31/2018.
  */
-class RegularBluetoothDeviceSearcher(private val useCaseComponent: UseCaseComponent) {
+class RegularBluetoothDeviceSearcher(private val useCaseComponent: UseCaseComponent
+                                     , private val dataListener: ObdManager.IBluetoothDataListener
+                                     , private val context: Context
+                                     , private val manager: BluetoothDeviceManager) {
 
     private val TAG = RegularBluetoothDeviceSearcher::class.java.simpleName
 
-    private val bluetoothAdapter: BluetoothAdapter? = null
+    private var bluetoothAdapter: BluetoothAdapter
     private var discoveryNum = 0
     private var nonUrgentScanInProgress = false
     private var discoveryWasStarted = false
@@ -27,26 +41,91 @@ class RegularBluetoothDeviceSearcher(private val useCaseComponent: UseCaseCompon
     private var rssiScan = false
     private val foundDevices = HashMap<BluetoothDevice, Short>() //Devices found by receiver
 
+    // for classic discovery
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+
+            //Found device
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                val rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, java.lang.Short.MIN_VALUE)
+                Log.d(TAG, "name: " + device.name + ", address: " + device.address + " RSSI: " + rssi)
+                //Store all devices in a map
+                if (device.name != null && (device.name.contains(ObdManager.BT_DEVICE_NAME) ||
+                                device.name.equals(ObdManager.CARISTA_DEVICE, ignoreCase = true)
+                                || device.name.equals(ObdManager.VIECAR_DEVICE, ignoreCase = true)
+                                || device.name.equals(ObdManager.OBDII_DEVICE_NAME, ignoreCase = true)
+                                || device.name.equals(ObdManager.OBD_LINK_MX, ignoreCase = true))
+                        && !foundDevices.containsKey(device)) {
+                    foundDevices[device] = rssi
+                    Log.d(TAG, "foundDevices.put() device name: " + device.name)
+                } else {
+                    Log.d(TAG, "Device did not meet criteria for foundDevice list name=" + device.name)
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
+                discoveryWasStarted = false
+                discoveryNum++
+                Logger.getInstance()!!.logI(TAG, "Discovery finished", DebugMessage.TYPE_BLUETOOTH)
+
+                //Connect to device with strongest signal if scan has been requested
+                if (rssiScan) {
+                    rssiScan = false
+                    dataListener.onDevicesFound()
+                    var foundDevicesString = "{"
+                    for ((key, value) in foundDevices) {
+                        foundDevicesString += key.name + "=" + value + ","
+                    }
+                    foundDevicesString += "}"
+
+                    Logger.getInstance()!!.logI(TAG, "Found devices: $foundDevicesString", DebugMessage.TYPE_BLUETOOTH)
+                    if (foundDevices.size > 0) {
+                        //Try to connect to available device, if none qualify then finish scan
+                        if (!connectToNextDevice()) dataListener.scanFinished()
+                    } else {
+                        //Notify scan finished after 0.5 seconds due to delay
+                        // in receiving CONNECTING notification
+                        dataListener.scanFinished()
+                    }
+                }
+
+            }//Finished scanning
+        }
+    }
+
+    init{
+        // for classic discovery
+        val intentFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        context.registerReceiver(receiver, intentFilter)
+        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
+    }
+
     @Synchronized
     fun startScan(urgent: Boolean, ignoreVerification: Boolean): Boolean {
         Log.d(TAG, "startScan() urgent: " + java.lang.Boolean.toString(urgent)
                 + " ignoreVerification: " + java.lang.Boolean.toString(ignoreVerification))
         this.ignoreVerification = ignoreVerification
-        if (!bluetoothAdapter!!.isEnabled && !urgent) {
+        if (!bluetoothAdapter.isEnabled && !urgent) {
             Log.i(TAG, "Scan unable to start, bluetooth is disabled and non urgent scan")
             return false
-        } else if (!bluetoothAdapter?.isEnabled && urgent) {
+        } else if (!bluetoothAdapter.isEnabled && urgent) {
             Log.d(TAG, "urgent scan, bluetooth is enabled()")
             bluetoothAdapter.enable() //Enable bluetooth, scan will be triggered someplace else
             return false
         }
 
-        if (bluetoothAdapter?.isDiscovering == true) {
+        if (bluetoothAdapter.isDiscovering) {
             Log.i(TAG, "Already scanning")
             return false
         }
 
         return connectBluetooth(urgent)
+    }
+
+    fun changeScanUrgency(urgent: Boolean) {
+        this.nonUrgentScanInProgress = !urgent
     }
 
     @Synchronized
@@ -64,7 +143,7 @@ class RegularBluetoothDeviceSearcher(private val useCaseComponent: UseCaseCompon
             return false
         }
 
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+        if (bluetoothAdapter.isEnabled) {
             Log.i(TAG, "Bluetooth not enabled or BluetoothAdapt is null")
             return false
         }
@@ -89,5 +168,118 @@ class RegularBluetoothDeviceSearcher(private val useCaseComponent: UseCaseCompon
             return false
         }
     }
+
+    //Returns whether a device qualified for connection
+    @Synchronized
+    fun connectToNextDevice(): Boolean {
+        Log.d(TAG, "connectToNextDevice(), foundDevices count: " + foundDevices.keys.size)
+
+        val minRssiThreshold: Short
+        var strongestRssi = java.lang.Short.MIN_VALUE
+        var strongestRssiDevice: BluetoothDevice? = null
+
+        if (nonUrgentScanInProgress) {
+            minRssiThreshold = -70
+
+        } else {
+            minRssiThreshold = java.lang.Short.MIN_VALUE
+        }//Deliberate scan, connect regardless
+
+        Log.d(TAG, "minRssiThreshold set to: $minRssiThreshold")
+
+        for ((key, value) in foundDevices) {
+            if (value != null && value > strongestRssi) {
+                strongestRssiDevice = key
+                strongestRssi = value
+            }
+        }
+        Log.d(TAG, "strongest rssi: $strongestRssi")
+        if (strongestRssiDevice == null || strongestRssi < minRssiThreshold) {
+            Log.d(TAG, "No device was found as candidate for a potential connection.")
+            foundDevices.clear()
+            return false
+
+        }
+
+        //Close previous connection
+        if (deviceInterface != null) {
+            deviceInterface?.closeConnection()
+        }
+
+        if (strongestRssiDevice.name.contains(ObdManager.BT_DEVICE_NAME_212)) {
+            Log.d(TAG, "device212 > RSSI_Threshold, device: $strongestRssiDevice")
+
+            foundDevices.remove(strongestRssiDevice)
+            connectTo212Device(strongestRssiDevice)
+        } else if (strongestRssiDevice.name.contains(ObdManager.BT_DEVICE_NAME_215)) {
+            Log.d(TAG, "device215 > RSSI_Threshold, device: $strongestRssiDevice")
+            foundDevices.remove(strongestRssiDevice)
+            connectTo215Device(strongestRssiDevice)
+        } else if (strongestRssiDevice.name.contains(ObdManager.CARISTA_DEVICE) ||
+                strongestRssiDevice.name.contains(ObdManager.VIECAR_DEVICE) ||
+                strongestRssiDevice.name.contains(ObdManager.OBDII_DEVICE_NAME) ||
+                strongestRssiDevice.name.contains(ObdManager.OBD_LINK_MX)) {
+            Log.d(TAG, "CaristaDevice> RSSI_Threshold, device: $strongestRssiDevice")
+            foundDevices.remove(strongestRssiDevice)
+            connectToELMDevice(strongestRssiDevice)
+        }
+
+        return true
+    }
+
+    private fun connectTo212Device(device: BluetoothDevice) {
+        Log.d(TAG, "connectTo212Device() device: " + device.name)
+        deviceInterface = Device212B(context, dataListener, device.name, manager)
+
+        deviceInterface?.connectToDevice(device)
+    }
+
+    private fun connectTo215Device(device: BluetoothDevice) {
+        Log.d(TAG, "connectTo215Device() device: " + device.name)
+        deviceInterface = Device215B(context, dataListener, device.name, manager)
+
+        deviceInterface?.connectToDevice(device)
+
+    }
+
+    private fun connectToELMDevice(device: BluetoothDevice) {
+        Log.d(TAG, "connectToELM327Device() device: " + device.name)
+        deviceInterface = ELM327Device(context, manager)
+        dataListener.setDeviceName(device.address)
+        deviceInterface?.connectToDevice(device)
+    }
+
+    fun moreDevicesLeft(): Boolean {
+        return foundDevices.size > 0
+    }
+
+    @Synchronized
+    fun onConnectDeviceValid() {
+        if (bluetoothAdapter.isEnabled && bluetoothAdapter.isDiscovering) {
+            Log.i(TAG, "Stopping scan")
+            bluetoothAdapter.cancelDiscovery()
+            dataListener.scanFinished()
+        }
+    }
+
+    fun close() {
+        Log.d(TAG, "close()")
+        btConnectionState = IBluetoothCommunicator.DISCONNECTED
+        if (bluetoothAdapter != null && bluetoothAdapter.isDiscovering) {
+            bluetoothAdapter.cancelDiscovery()
+            dataListener.scanFinished()
+        }
+
+        if (deviceInterface != null) {
+            deviceInterface?.closeConnection()
+        }
+        try {
+            context.unregisterReceiver(receiver)
+        } catch (e: Exception) {
+            Log.d(TAG, "Receiver not registered")
+        }
+
+    }
+
 
 }
