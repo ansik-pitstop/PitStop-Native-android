36ffbb71e6931bbea07a9c23b5525aa342276117
Karol <karol@pitstopconnect.com>
2018-08-31 17:36:56 -0400

    moved bluetooth search and connect logic to RegularBluetoothDeviceSearcher from BluetoothDeviceManager

--- app/src/pitstop/java/com/pitstop/bluetooth/bleDevice/RVDDevice.kt
+++ app/src/pitstop/java/com/pitstop/bluetooth/bleDevice/RVDDevice.kt
@@ -8,58 +8,76 @@ import java.util.*
  */
 class RVDDevice: AbstractDevice {
 
+    //Remove
     override fun getServiceUuid(): UUID {
     }
 
+    //Remove
     override fun getReadChar(): UUID {
     }
 
+    //Remove
     override fun getWriteChar(): UUID {
     }
 
+    //Remove
     override fun getBytes(payload: String?): ByteArray {
     }
 
+    //Remove
     override fun parseData(data: ByteArray?) {
     }
+
 
     override fun onConnectionStateChange(state: Int) {
     }
 
+    //Remove
     override fun requestData() {
     }
 
     override fun getVin(): Boolean {
+        return false
     }
 
     override fun getRtc(): Boolean {
+        return false
     }
 
     override fun setRtc(rtcTime: Long): Boolean {
+        return false
     }
 
     override fun getPids(pids: String?): Boolean {
+        return false
     }
 
     override fun getSupportedPids(): Boolean {
+        return false
     }
 
     override fun setPidsToSend(pids: String?, timeInterval: Int): Boolean {
+        return false
     }
 
     override fun requestSnapshot(): Boolean {
+        return false
     }
 
     override fun clearDtcs(): Boolean {
+        return false
     }
 
     override fun getDtcs(): Boolean {
+        return false
     }
 
     override fun getPendingDtcs(): Boolean {
+        return false
     }
 
     override fun getFreezeFrame(): Boolean {
+        return false
     }
 
     override fun connectToDevice(device: BluetoothDevice?): Boolean {
