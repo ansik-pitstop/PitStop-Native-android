fc18bdd6203a06fc4db4a4fede54420804638fb0
Breno Xavier <brenoxp2008@gmail.com>
2022-05-03 14:42:35 -0400

    Check for null context when accessing the stored values

--- app/src/pitstop/java/com/pitstop/application/GlobalVariables.kt
+++ app/src/pitstop/java/com/pitstop/application/GlobalVariables.kt
@@ -2,21 +2,32 @@ package com.pitstop.application
 
 import android.content.Context
 import androidx.preference.PreferenceManager
+import com.pitstop.models.DebugMessage
+import com.pitstop.utils.Logger
 
 
 class GlobalVariables {
     companion object {
+        private const val TAG = "GlobalVariables"
         private const val carIdKey = "carId"
         private const val userIdKey = "userId"
 
-        fun getMainCarId(context: Context): Int? {
+        fun getMainCarId(context: Context?): Int? {
+            if (context == null) {
+                Logger.getInstance().logE(TAG,"getMainCarId: context is null", DebugMessage.TYPE_OTHER)
+                return null
+            }
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             val carId = preferences.getInt(carIdKey, -1)
             if (carId == -1) return null
             return carId
         }
 
-        fun setMainCarId(context: Context, carId: Int?) {
+        fun setMainCarId(context: Context?, carId: Int?) {
+            if (context == null) {
+                Logger.getInstance().logE(TAG,"setMainCarId: context is null", DebugMessage.TYPE_OTHER)
+                return
+            }
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             val editor = preferences.edit()
             if (carId == null) {
